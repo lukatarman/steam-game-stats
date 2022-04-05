45213dd9853922fe3a73fce3eb30d9a72bd5dda6
Stanislav Jakuschevskij <stanislav.jakuschevskij@at.ibm.com>
2022-04-05 16:09:04 +0200

    added first fetch of data

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('start steam game stats runtime - entry point');
+const axios = require('axios');
+
+axios.get('https://api.steampowered.com/ISteamApps/GetAppList/v1')
+  .then((response) => {
+    // handle success
+    console.log('List of games from steam:', response.data);
+  })
+  .catch((error) => {
+    // handle error
+    console.log(error);
+  })
+  .then(() => {
+    // always executed
+  });
+
+setTimeout(() => console.log('start steam game stats runtime - entry point'), 5000);
