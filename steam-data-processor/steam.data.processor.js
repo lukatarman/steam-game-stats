f167471babb91f65a50383c4e6c8e567e9aa3e80
Luka Tarman <luka.tarman@gmail.com>
2022-04-08 20:14:25 +0200

    added processor
    # implemented fetching, filtering, storing games
    # added filter mock
    # added sanitize games mock
    # added pseudo code method description

---
+++ steam-data-processor/steam.data.processor.js
@@ -0,0 +1,28 @@
+export class SteamDataProcessor {
+  #databaseClient;
+  #httpClient;
+
+  constructor(httpClient, databaseClient) {
+    this.#httpClient = httpClient;
+    this.#databaseClient = databaseClient;
+  }
+
+  async createGamesList() {
+    const options = { params: { key: "79E04F52C6B5AD21266624C05CC12E42" } };
+    const url = "https://api.steampowered.com/ISteamApps/GetAppList/v2";
+    const games = await axios.get(url, options).data.applist.apps;
+    const filteredGames = this.#filterNonGamesMOCK(games);
+    await this.#databaseClient.insertMany("games", filteredGames);
+  }
+
+  #filterNonGamesMOCK(games) {
+    return games;
+  }
+
+  async sanitizeGamesListMOCK() {
+    // const games = await this.#databaseClient.getAll("games", { game: [ 'true', 'undefined' ] });
+    // for each game call steamcharts and check if the game has a page - here we will need a page crawler
+    // update game object property "game" with true or false
+    // store each game to the database
+  }
+}
