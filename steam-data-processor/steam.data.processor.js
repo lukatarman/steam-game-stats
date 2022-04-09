e75eeb91d4ce52810a08444858b8de8d97e0c5fc
Luka Tarman <luka.tarman@gmail.com>
2022-04-09 10:45:02 +0200

    added filterNonGames function
    # added filterByGameName function

--- steam-data-processor/steam.data.processor.js
+++ steam-data-processor/steam.data.processor.js
@@ -11,12 +11,30 @@ export class SteamDataProcessor {
     const options = { params: { key: "79E04F52C6B5AD21266624C05CC12E42" } };
     const url = "https://api.steampowered.com/ISteamApps/GetAppList/v2";
     const games = await this.#httpClient.get(url, options).data.applist.apps;
-    const filteredGames = this.#filterNonGamesMOCK(games);
+    const filteredGames = this.#filterNonGames(games);
     await this.#databaseClient.insertMany("games", filteredGames);
   }
 
-  #filterNonGamesMOCK(games) {
+  #filterNonGames(games) {
+    for (let game of games) {
+      if (this.#filterByGameName(game)) {
+        game.isGame = false;
+      }
+    }
     return games;
+  }
+
+  //The names for non games is very inconsistent, only included ones that are for sure not games
+  #filterByGameName(game) {
+    if (game.isGame === undefined) {
+      const lowercaseGameName = game.name.toLowerCase();
+
+      if (lowercaseGameName.match(/dlc$/)) return true;
+
+      if (lowercaseGameName.match(/soundtrack$/)) return true;
+
+      return false;
+    }
   }
 
   async sanitizeGamesListMOCK() {
