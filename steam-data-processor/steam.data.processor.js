2affb2ed8822bee04668a42794a1223f57e96b50
Luka Tarman <luka.tarman@gmail.com>
2022-04-09 09:48:13 +0200

    get request changed to this.#httpClient

--- steam-data-processor/steam.data.processor.js
+++ steam-data-processor/steam.data.processor.js
@@ -10,7 +10,7 @@ export class SteamDataProcessor {
   async createGamesList() {
     const options = { params: { key: "79E04F52C6B5AD21266624C05CC12E42" } };
     const url = "https://api.steampowered.com/ISteamApps/GetAppList/v2";
-    const games = await axios.get(url, options).data.applist.apps;
+    const games = await this.#httpClient.get(url, options).data.applist.apps;
     const filteredGames = this.#filterNonGamesMOCK(games);
     await this.#databaseClient.insertMany("games", filteredGames);
   }
