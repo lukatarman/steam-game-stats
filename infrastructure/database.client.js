881c51351668779560666fa413bd9b72b433091d
Luka Tarman <luka.tarman@gmail.com>
2022-04-09 20:41:18 +0200

    Restored back to my version

---
+++ infrastructure/database.client.js
@@ -0,0 +1,47 @@
+import { MongoClient } from "mongodb";
+
+export class DatabaseClient {
+  #collections;
+
+  async init(
+    options = {
+      url: "mongodb://localhost:27017",
+      databaseName: "game-stats",
+      collections: ["games"],
+    }
+  ) {
+    const mongodb = await MongoClient.connect(options.url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    const database = mongodb.db(options.databaseName);
+
+    this.#collections = new Map();
+    options.collections.forEach((c) =>
+      this.#collections.set(c, database.collection(c))
+    );
+
+    return this;
+  }
+
+  async insertMany(collectionName, data) {
+    const insertResult = await this.#collections
+      .get(collectionName)
+      .insertMany(data);
+    console.log("Inserted documents =>", insertResult);
+  }
+
+  async getAll(collectionName, options = {}) {
+    const getAllResults = await this.#collections
+      .get(collectionName)
+      .find(options);
+  }
+
+  async updateOne(collectionName, filter, update) {
+    const updateResults = await this.collection
+      .get(collectionName)
+      .updateOne(filter, options);
+    console.log("Matched document =>", updateResults.matchedCount);
+  }
+}
---
+++ models/players.js
@@ -0,0 +1,5 @@
+export class Players {
+  date;    // date=new Date('exact time when player numbers are collected');
+  players; // players=0..n
+}
+
