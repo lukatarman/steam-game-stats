8a0a222addd0c7eb8f54dfb83305691756e31d45
Luka Tarman <luka.tarman@gmail.com>
2022-04-10 17:11:29 +0200

    updated game model

--- src/models/game.js
+++ src/models/game.js
@@ -1,9 +1,8 @@
 // data model which will be stored to the database and represents one game
 export class Game {
-  appid; // appid=123456
+  id; // appid=123456
   name; // name="Metro"
-  isGame; // game=true|false
-  imgUrl; // imgUrl="https://www.some.url.com/gameimgname.png"
-  imgData; // imgData=['D%', '7Z', 'HJ', ')I', 'LK'...'M;'];
+  imageUrl; // imgUrl="https://www.some.url.com/gameimgname.png"
+  image; // imgData=['D%', '7Z', 'HJ', ')I', 'LK'...'M;'];
   playerHistory; // playerHistory=[{ date: "05.04.22", players: 122 }, { date: "04.04.22", players: 200 }, { date: "03.04.22", players: 150 }]
 }
