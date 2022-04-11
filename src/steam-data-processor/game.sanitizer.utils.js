ea6fa594db2ab237bac08535fd11723405bf4293
Luka Tarman <luka.tarman@gmail.com>
2022-04-11 12:03:33 +0200

    refactored game.sanitizer.utils.js

--- src/steam-data-processor/game.sanitizer.utils.js
+++ src/steam-data-processor/game.sanitizer.utils.js
@@ -1,14 +1,19 @@
 import { crawlCheckIfGameMOCK } from "./website.crawler";
 
-export async function sanitizeGamesList(gamesNameFiltered, removeNonGames) {
-  let games = [];
+export async function sanitizeGamesList(gamesNameFiltered) {
   // for each game call steamcharts and check if the game has a page - here we will need a page crawler
-  gamesNameFiltered.forEach((gameNameFiltered) => {
-    if (await crawlCheckIfGameMOCK) {
-      gameNameFiltered.push(games);
-    }
-  });
   // update game object property "game" with true or false
   // store each game to the database
-  return games;
+
+  // let games = [];
+  // gamesNameFiltered.forEach((gameNameFiltered) => {
+  //   if (await crawlCheckIfGameMOCK) {
+  //     gameNameFiltered.push(games);
+  //   }
+  // });
+  // return games;
+
+  return gamesNameFiltered.filter(
+    (gameNameFiltered) => await crawlCheckIfGameMOCK(gameNameFiltered)
+  );
 }
