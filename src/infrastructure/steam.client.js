4e48b4aefc7b350f94d3f6464e63bc0d52e179f9
Luka Tarman <luka.tarman@gmail.com>
2022-04-12 11:10:21 +0200

    added getCurrentPlayers function
    # added to steam.client.sj

--- src/infrastructure/steam.client.js
+++ src/infrastructure/steam.client.js
@@ -11,4 +11,11 @@ export class SteamClient {
 
     return this.#httpClient.get(url, options).data.applist.apps;
   }
+
+  getCurrentPlayers(game) {
+    const url = `https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid=${game.id}`;
+    const options = { params: { key: "79E04F52C6B5AD21266624C05CC12E42" } };
+
+    return this.#httpClient.get(url, options).data.response.player_count;
+  }
 }
