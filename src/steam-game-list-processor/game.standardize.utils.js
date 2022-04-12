4d66da91e46b75d41b526524460c66b93f71ee6f
Luka Tarman <luka.tarman@gmail.com>
2022-04-12 10:55:39 +0200

    changed name of game.standardize.utils.js
    # changed to game.data.model.utils.js

--- src/steam-game-list-processor/game.standardize.utils.js
+++
@@ -1,9 +0,0 @@
-export function standardizeDataModel(gamesNotStandardized) {
-  gamesNotStandardized.map(standardizeGames);
-
-  function standardizeGames(game) {
-    game = { ...game, id: game.appid };
-    delete game.appid;
-    return game;
-  }
-}
