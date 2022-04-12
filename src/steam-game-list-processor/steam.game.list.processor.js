f1b5b1709748ccde01a245f024a75715bb062ffc
Luka Tarman <luka.tarman@gmail.com>
2022-04-12 19:52:26 +0200

    refactored identifyGames method
    # removed comments
    # impletemented filterSteamAppsByAppType method
    # removed deprecated imports

--- src/steam-game-list-processor/steam.game.list.processor.js
+++ src/steam-game-list-processor/steam.game.list.processor.js
@@ -1,9 +1,9 @@
-import { filterGamesByName } from "./game.filter.utils";
-import { addImageLinks } from "./game.image.utils";
-import { sanitizeGamesList } from "./game.sanitizer.utils";
-import { standardizeDataModel } from "./game.standardize.data.model.utils";
+import {
+  filterSteamAppsByName,
+  steamAppIsAgameMOCK,
+} from "./steam.app.utils.js";
+import { Game } from "../models/game.js";
 
-// rename to GameData
 export class SteamGameListProcessor {
   #steamClient;
   #databaseClient;
@@ -19,23 +19,29 @@ export class SteamGameListProcessor {
   }
 
   async #getAllSteamApps() {
-    // get all the steam apps
-    // store them to the db into the new collection
     const steamApps = await this.#steamClient.getAppList();
     await this.#databaseClient.insertMany("steam_apps", steamApps);
   }
 
   async #identifyGames() {
-    // get all steam apps from our db
-    // one by one check is it a game
-    // if its a game, store it in the games collection
-    // and get missing data { imageUrl, image } https://cdn.akamai.steamstatic.com/steam/apps/APPID/header.jpg
     const steamApps = await this.#databaseClient.getAll("steam_apps");
-    const gamesNameFiltered = filterGamesByName(steamApps);
-    const gamesNotStandardized = sanitizeGamesList(gamesNameFiltered);
-    const gamesNoImg = standardizeDataModel(gamesNotStandardized);
-    const games = addImageLinks(gamesNoImg);
+    const filteredSteamApps = filterSteamAppsByName(steamApps);
+    const games = this.#filterSteamAppsByAppType(filteredSteamApps);
 
     this.#databaseClient.insertMany("games", games);
+  }
+
+  async #filterSteamAppsByAppType(steamApps) {
+    const games = [];
+
+    for (steamApp in steamApps) {
+      const steamAppDetailPageAsString = this.#steamClient.getAppDetailsPage(
+        steamApp.appid
+      );
+
+      if (steamAppIsAgameMOCK(steamAppDetailPageAsString)) games.push(new Game(steamApp));
+    }
+
+    return games;
   }
 }
