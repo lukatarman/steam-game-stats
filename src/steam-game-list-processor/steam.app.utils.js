af6ef8d2e712310849a6c9e4c8172c75a7244ab8
Luka Tarman <luka.tarman@gmail.com>
2022-04-14 19:45:58 +0200

    added filter

--- src/steam-game-list-processor/steam.app.utils.js
+++ src/steam-game-list-processor/steam.app.utils.js
@@ -35,7 +35,7 @@ export function tagNonGames(steamApps) {
 }
 
 // check html page for details of the steam app if its a game or something else and return true or false
-export function steamAppIsGameMOCK(steamApp, steamClient) {
+export function steamAppIsGameWIP(steamApp, steamClient) {
   const steamAppDetailPageAsString = await steamClient.getAppDetailsPage(steamApp);
   const steamAppDetailPageAsDom = new JSDOM(steamAppDetailPageAsString).data;
   const breadCrumbClass =
@@ -60,4 +60,19 @@ function checkIfGameThroughSteamCharts(steamApp, steamClient) {
 
   if (steamChartsAppDetailsAsDom) return true;
   return false;
+}
+
+export function steamAppIsGame(detailsPage) {
+  const dom = new JSDOM(detailsPage).data;
+  const breadcrumbElement = dom.window.document.querySelector(".blockbg");
+
+  if (!breadcrumbElement) return false;
+
+  const breadcrumbText = breadcrumbElement.children[0].textContent;
+
+  if (breadcrumbText !== "All Software" && breadcrumbText !== "All Games") return false;
+
+  const indexOfDlc = breadcrumbElement.children.indexOf(child => child.textContent === 'Downloadable Content');
+
+  return indexOfDlc === -1 ? false : true;
 }
