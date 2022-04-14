9cec7120feab6a52dfd8f7776054fbd6ab0e5231
Luka Tarman <luka.tarman@gmail.com>
2022-04-14 15:06:19 +0200

    refactored testing to test for different code

--- src/infrastructure/steam.client.spec.js
+++ src/infrastructure/steam.client.spec.js
@@ -1,14 +1,33 @@
 import axios from "axios";
+import { JSDOM } from "jsdom";
 import { SteamClient } from "./steam.client.js";
 
-xdescribe('steam.client.js', () => {
+describe("steam.client.js", () => {
   const steamClient = new SteamClient(axios);
 
-  it('fetch html page of metro',  async () => {
-    const page = await steamClient.getAppDetailsPage(412020);
+  it("fetch html page of metro", async () => {
+    // const page = await steamClient.getAppDetailsPage(12220);
 
-    console.log(page);
+    // console.log("Showing Metro page:");
+    // console.log(page.data);
 
-    expect(page).toBeDefined();
+    // const appDetilsAsString = new JSDOM(page.data);
+    // const testText = appDetilsAsString.window.document.querySelector(".blockbg").children[0].textContent;
+
+    const steamChartsAppDetailsAsString = await steamClient
+      .getAppDetailsSteamCharts({ id: 829050 })
+      .catch(function (error) {
+        console.log("ERROR DETECTED");
+      });
+    const steamChartsAppDetailsAsDom = new JSDOM(steamChartsAppDetailsAsString).data;
+    // const myH1 = steamChartsAppDetailsAsDom.querySelector(".content-wrapper .header h1");
+    // const myText = myH1.textContent;
+
+    console.log("this is the result:");
+    console.log(steamChartsAppDetailsAsDom);
+    console.log(myText);
+    // console.log(myText);
+
+    expect(steamChartsAppDetailsAsDom).toBeDefined();
   });
 });
