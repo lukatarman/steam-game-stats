33983bdf3337ed3f432fd068cb8f470bbc8a6a23
Stanislav Jakuschevskij <stanislav.jakuschevskij@at.ibm.com>
2022-04-17 16:10:28 +0200

    added delayed func runner

--- src/steam-data-aggregator/services/time.service.js
+++ src/steam-data-aggregator/services/time.service.js
@@ -10,3 +10,18 @@ export function delay(ms) {
   ms = ms || 2000;
   return new Promise(done => setTimeout(done, ms));
 }
+
+export function runFuncWithDelayOfXhours(func, x) {
+  let firstRun = true;
+  while(true) {
+    if (firstRun) {
+      firstRun = false;
+      const tillNextRun = hours > x ? hours - x : x - hours;
+      await delay(tillNextRun * 36e5);
+    }
+
+    func();
+
+    await delay(x * 36e5);
+  }
+}
