440bcbd5427d422a1ccb0f1fb0d4c931e5c26af4
Stanislav Jakuschevskij <stanislav.jakuschevskij@at.ibm.com>
2022-04-17 16:01:09 +0200

    added time service

---
+++ src/steam-data-aggregator/services/time.service.js
@@ -0,0 +1,12 @@
+export function moreThanXhoursPassedSince(x, date) {
+  const now = new Date();
+  const milliseconds = Math.abs(now - date);
+  const hours = milliseconds / 36e5;
+
+  return hours > x;
+}
+
+export function delay(ms) {
+  ms = ms || 2000;
+  return new Promise(done => setTimeout(done, ms));
+}
