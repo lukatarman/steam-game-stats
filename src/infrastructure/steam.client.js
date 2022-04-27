96aa4401e6ff9f8f548b38150b03c90b993d1bf1
Luka Tarman <luka.tarman@gmail.com>
2022-04-27 11:12:12 +0200

    testing if everything works on laptop

--- src/infrastructure/steam.client.js
+++ src/infrastructure/steam.client.js
@@ -5,6 +5,8 @@ export class SteamClient {
     this.#httpClient = httpClient;
   }
 
+  //teste commit
+
   getAppList() {
     const url = "https://api.steampowered.com/ISteamApps/GetAppList/v2";
     const options = { params: { key: "79E04F52C6B5AD21266624C05CC12E42" } };
