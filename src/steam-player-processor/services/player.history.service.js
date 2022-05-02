7acc5d907d7dc33a1a1c2293b39ab79ae089763f
Luka Tarman <luka.tarman@gmail.com>
2022-05-02 14:27:20 +0200

    added player history service file

---
+++ src/steam-player-processor/services/player.history.service.js
@@ -0,0 +1,28 @@
+import { JSDOM } from "jsdom";
+
+export function parsePlayerHistory(pageHttpDetails) {
+    const dom = new JSDOM(pageHttpDetails.data);
+    const playerHistoryEntries = dom.window.document.querySelector(".common-table tbody").children;
+    const playerHistories = [];
+
+    for(let entry of playerHistoryEntries) {
+        if(entry[0].textContent === "Last 30 Days") continue;
+    
+        const monthAndYear = entry[0].textContent.split(" ");
+        const month = monthAndYear[0];
+        const year = monthAndYear[1];
+
+        const averagePlayers = parseInt(entry[1].textContent);
+
+        const date = new Date(`${month} ${year}`);
+
+        const playerHistory = {
+            date: date,
+            players: averagePlayers,
+        }
+
+        playerHistories.push(playerHistory);
+    }
+
+    return playerHistories;
+}
