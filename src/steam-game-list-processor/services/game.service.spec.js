0be881a26dce061e5a29648eb41bf65e5d00d57a
Stanislav Jakuschevskij <stanislav.jakuschevskij@at.ibm.com>
2022-05-05 08:51:54 +0200

    added positive unit test

--- src/steam-game-list-processor/services/game.service.spec.js
+++ src/steam-game-list-processor/services/game.service.spec.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 import { gamesMock } from "../../../assets/small.data.set.js";
-import { steamAppIsGame, filterSteamAppsByName, tagNonGames } from "./game.service.js";
+import { steamAppIsGame, filterSteamAppsByName, tagNonGames, identifyGamesFromSteamHtmlDetailsPages } from "./game.service.js";
 import { SteamClient } from "../../infrastructure/steam.client.js";
+import { featressGameHtmlDetailsPage } from "../../../assets/steam-details-pages/feartress.game.html.detals.page.js";
+import { goAceItGameHtmlDetailsPage } from "../../../assets/steam-details-pages/go.ace.it.game.html.details.page.js";
+import { animaddicts2gameHtmlDetailsPage } from "../../../assets/steam-details-pages/animaddicts.2.game.html.detail.page.js";
+import { mortalDarknessGameHtmlDetailsPage } from "../../../assets/steam-details-pages/mortal.darkness.game.html.details.page.js";
+import { glitchhikersSoundtrackHtmlDetailsPage } from "../../../assets/steam-details-pages/glitchhikers.soundtrack.html.details.page.js";
 
 describe("game.service.js", () => {
   describe(".filterSteamAppsByName", () => {
@@ -195,12 +200,38 @@ describe("game.service.js", () => {
   });
 
   describe(".identifyGamesFromSteamHtmlDetailsPages", () => {
+    let steamApps;
+    let htmlDetailsPages;
+    let games;
+    let identifiedPages;
+
     describe("identifies one game successfully", () => {
-      it("returns an array of games with length 1", () => {});
-      it("the game has the same id as the steam app", () => {});
-      it("the game has the same name as the steam app", () => {});
-      it("returns an array of identifiedPages with the same length as htmlDetailsPages", () => {});
-      it("the identified game page has the string 'identified' in its place in the array", () => {});
+      beforeAll(() => {
+        steamApps = smallSteamAppsDataset()
+        htmlDetailsPages = smallHtmlDetailsPagesDataset();
+
+        [games, identifiedPages] = identifyGamesFromSteamHtmlDetailsPages(steamApps, htmlDetailsPages);
+      });
+
+      it("returns an array of games with length 1", () => {
+        expect(games.length).toBe(1);
+      });
+
+      it("the game has the same id as the steam app", () => {
+        expect(games[0].id).toBe(steamApps[3].appid);
+      });
+
+      it("the game has the same name as the steam app", () => {
+        expect(games[0].name).toBe(steamApps[3].name);
+      });
+
+      it("returns an array of identifiedPages with the same length as htmlDetailsPages", () => {
+        expect(identifiedPages.length).toBe(htmlDetailsPages.length);
+      });
+
+      it("the identified game page has the string 'identified' in its place in the array", () => {
+        expect(identifiedPages[3]).toBe('identified');
+      });
     });
 
     describe("identifies two games successfully", () => {
@@ -221,3 +252,37 @@ describe("game.service.js", () => {
     });
   });
 });
+
+function smallSteamAppsDataset() {
+  return [{
+      appid: 1904280,
+      name: "Feartress",
+    },
+    {
+      appid: 1904310,
+      name: "Go Ace It",
+    },
+    {
+      appid: 1904320,
+      name: "Animaddicts 2",
+    },
+    {
+      appid: 1904380,
+      name: "Mortal Darkness",
+    },
+    {
+      appid: 1898200,
+      name: "Glitchhikers: The Spaces Between Deluxe Soundtrack 5-Volume Set",
+    },
+  ];
+}
+
+function smallHtmlDetailsPagesDataset() {
+  return [
+    featressGameHtmlDetailsPage,
+    goAceItGameHtmlDetailsPage,
+    animaddicts2gameHtmlDetailsPage,
+    mortalDarknessGameHtmlDetailsPage,
+    glitchhikersSoundtrackHtmlDetailsPage,
+  ];
+}
