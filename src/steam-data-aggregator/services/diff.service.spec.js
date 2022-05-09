4c5022daebf0a0cc1ed36d1d7d1ecb4c11f65c79
Luka Tarman <luka.tarman@gmail.com>
2022-05-09 11:18:13 +0200

    added test

---
+++ src/steam-data-aggregator/services/diff.service.spec.js
@@ -0,0 +1,19 @@
+import { gamesMock } from "../../../assets/small.data.set.js"
+import { smallestGamesMock } from "../../../assets/smallest.data.set.js"
+import { diff } from "./diff.service.js"
+
+fdescribe("diff.service.js", () => {
+    describe(".diff", () => {
+        describe("passing in two arrays of objects, the function", () => {
+            let object;
+
+            beforeAll(() => {
+                object = diff(gamesMock, smallestGamesMock);
+            })
+
+            it("returns an array with the differences between the arrays", () => {
+                expect(object.length).toBe(205);
+            })
+        })
+    })
+})
