82d4a05858c41c40302cbc0fc3247e0614eeb5b5
Luka Tarman <luka.tarman@gmail.com>
2022-05-09 18:50:50 +0200

    commented out codet continue later

---
+++ src/shared/time.utils.spec.js
@@ -0,0 +1,8 @@
+// import { delay } from "./time.utils.js";
+
+// xdescribe("time.utils.js", () => {
+//   describe(".delay", () => {
+//     describe("waits for 100 ms", () => {});
+//     describe("waits for 500 ms", () => {});
+//   });
+// });
