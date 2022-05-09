517ca9489da747ee8c4303efe49b77d8c80d3645
Luka Tarman <luka.tarman@gmail.com>
2022-05-09 12:50:02 +0200

    added test for missing function

--- src/steam-data-aggregator/services/time.service.spec.js
+++ src/steam-data-aggregator/services/time.service.spec.js
@@ -1,9 +1,10 @@
-import { 
-  runFuncInLoopWithDelayOfXmsFromDate, 
+import {
   moreThanXhoursPassedSince ,
+  msPassedSince,
+  runFuncInLoopWithDelayOfXmsFromDate,
 } from "./time.service.js";
 
-describe("time.service.js", () => {
+fdescribe("time.service.js", () => {
   describe(".moreThanXhoursPassedSince", () => {
     describe("if the date value is larger than 'x' hours passed", () => {
       let result;
@@ -41,4 +42,22 @@ describe("time.service.js", () => {
       })
     })
   });
+
+  describe(".msPassedSince", () => {
+    describe("when a date is three hours behind the current time", () => {
+      let result;
+
+      beforeAll(() => {
+        const threeHoursInMs = 108e5;
+        const threeHoursBehind = new Date().getTime() - threeHoursInMs;
+        const date = new Date(threeHoursBehind);
+
+        result = msPassedSince(date);
+      })
+
+      it("function returns 10800000", () => {
+        expect(result).toBe(108e5);
+      })
+    })
+  })
 });
