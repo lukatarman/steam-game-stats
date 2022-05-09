066343e3f8a853fa8f1b5e244393ac0af05254fc
Luka Tarman <luka.tarman@gmail.com>
2022-05-09 11:07:55 +0200

    added test

--- src/steam-data-aggregator/services/time.service.spec.js
+++ src/steam-data-aggregator/services/time.service.spec.js
@@ -1,5 +1,44 @@
-describe('time.service.js', () => {
-  describe('runFuncInLoopWithDelayOfXhours', () => {
+import { 
+  runFuncInLoopWithDelayOfXmsFromDate, 
+  moreThanXhoursPassedSince ,
+} from "./time.service.js";
 
+fdescribe("time.service.js", () => {
+  describe(".moreThanXhoursPassedSince", () => {
+    describe("if the date value is larger than 'x' hours passed", () => {
+      let result;
+
+      beforeAll(() => {
+        const oneDayInMs = 864e5;
+        const threeHoursInMs = 108e5;
+        const oneDayFromNow = new Date().getTime() + oneDayInMs;
+        const date = new Date(oneDayFromNow);
+
+        result = moreThanXhoursPassedSince(threeHoursInMs, date);
+      })
+
+      it("function returns true", () => {
+        expect(result).toBe(true);
+      })
+    })
+  });
+
+  describe(".moreThanXhoursPassedSince", () => {
+    describe("if the date value is smaller than 'x' hours passed", () => {
+      let result;
+
+      beforeAll(() => {
+        const oneDayInMs = 864e5;
+        const threeHoursInMs = 108e5;
+        const threeHoursFromNow = new Date().getTime() + threeHoursInMs;
+        const date = new Date(threeHoursFromNow);
+
+        result = moreThanXhoursPassedSince(oneDayInMs, date);
+      })
+
+      it("function returns true", () => {
+        expect(result).toBe(false);
+      })
+    })
   });
 });
