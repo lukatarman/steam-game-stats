a08ba46cb6c05edf816e781aac945e967268b8d4
Luka Tarman <luka.tarman@gmail.com>
2022-05-13 13:17:21 +0200

    added running steamGameListProcessor

--- src/main.js
+++ src/main.js
@@ -24,11 +24,11 @@ async function main() {
 
   };
   const steamDataAggregator = new SteamDataAggregator(steamClient, databaseClient, options);
-  // const steamGameListProcessor = new SteamGameListProcessor(steamClient, databaseClient, options);
+  const steamGameListProcessor = new SteamGameListProcessor(steamClient, databaseClient, options);
 
   // run phase
-  steamDataAggregator.run();
-  // steamGameListProcessor.run();
+  await steamDataAggregator.run();
+  steamGameListProcessor.run();
 }
 
 main();
