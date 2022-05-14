d5d3541a4fbc774bd569b702eccc71ad124aed9d
Luka Tarman <luka.tarman@gmail.com>
2022-05-14 19:14:37 +0200

    fixed bugs, typos

--- src/steam-data-aggregator/services/label.service.spec.js
+++ src/steam-data-aggregator/services/label.service.spec.js
@@ -1,7 +1,7 @@
 import { labelAsNotIdentified } from "./label.service.js";
 import { gamesMock } from "../../../assets/small.data.set.js"
 
-fdescribe("label.service.js", () => {
+describe("label.service.js", () => {
   describe(".labelAsNotIdentified", () => {
     describe("if an array of objects is passed into the function", () => {
       let result;
@@ -9,17 +9,17 @@ fdescribe("label.service.js", () => {
       beforeAll(() => {
         const steamApps = gamesMock;
         result = labelAsNotIdentified(steamApps);
-      })
+      });
 
       it("object in the first index has its 'identified' property set to false", () => {
         expect(result[0].identified).toBeFalse();
-      })
+      });
       it("object in the 15th index has its 'identified' property set to false", () => {
         expect(result[15].identified).toBeFalse();
-      })
+      });
       it("object in the last index has its 'identified' property set to false", () => {
-        expect(result.pop().identified).toBeFalse();
-      })
-    })
-  })
-})
+        expect((result[result.length - 1]).identified).toBeFalse();
+      });
+    });
+  });
+});
