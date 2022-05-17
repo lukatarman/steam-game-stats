804d26ee2e3bb2fa428047f558dfcaea79101679
Stanislav Jakuschevskij <stanislav.jakuschevskij@at.ibm.com>
2022-05-17 19:10:30 +0200

    added parallel test case

--- src/runner/runner.spec.js
+++ src/runner/runner.spec.js
@@ -1,3 +1,4 @@
+import { delay } from "../shared/time.utils.js";
 import { Runner } from "./runner.js";
 
 fdescribe("runner.js", () => {
@@ -33,7 +34,35 @@ fdescribe("runner.js", () => {
     });
   });
 
-  xdescribe("runs multiple functions in parallel in a loop for one iteration", () => {});
+  describe("runs multiple functions in parallel in a loop for one iteration", () => {
+    let counterOne = 0;
+    let counterTwo = 0;
+    let passedTime;
+
+    beforeAll(() => {
+      const funcOne = async () => { counterOne++; await delay(500); };
+      const funcTwo = async () => { counterTwo++; await delay(500); };
+      const options = { iterations: 1 };
+      const runner = new Runner([funcOne, funcTwo], options);
+
+      const beforeStart = new Date();
+      runner.run();
+      const afterStart = new Date();
+      passedTime = afterStart.getTime() - beforeStart.getTime();
+    });
+
+    it("should increment coutner one by 1", () => {
+      expect(counterOne).toBe(1);
+    });
+
+    it("should increment coutner two by 1", () => {
+      expect(counterTwo).toBe(1);
+    });
+
+    it("should run in parallel", () => {
+      expect(passedTime).toBeLessThan(1000);
+    });
+  });
 
   xdescribe("runs multiple functions in parallel in a loop for multiple iterations", () => {});
 });
