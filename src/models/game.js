76511ff594b57e3ba314b62afb583558843f8807
Luka Tarman <luka.tarman@gmail.com>
2022-05-17 12:20:08 +0200

    added comment on playerHistory

--- src/models/game.js
+++ src/models/game.js
@@ -5,6 +5,7 @@ export class Game {
   image; // imgData=['D%', '7Z', 'HJ', ')I', 'LK'...'M;'];
   imageUrl; // imageUrl=`https://cdn.akamai.steamstatic.com/steam/apps/${this.id}/header.jpg`
   playerHistory; // playerHistory=[{ date: "05.04.22", players: 122 }, { date: "04.04.22", players: 200 }, { date: "03.04.22", players: 150 }]
+  // playerHistory is set to an empty array if is has been checked on steamcharts but returned no info on the player history.
 
   constructor(steamApp) {
     this.id = steamApp.appid;
