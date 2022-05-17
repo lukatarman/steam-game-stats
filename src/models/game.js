9c78e474ae74ecf389640be965acb99da66b1e37
Luka Tarman <luka.tarman@gmail.com>
2022-05-17 11:20:15 +0200

    fixed typo, added mode comment

--- src/models/game.js
+++ src/models/game.js
@@ -3,12 +3,12 @@ export class Game {
   id; // appid=123456
   name; // name="Metro"
   image; // imgData=['D%', '7Z', 'HJ', ')I', 'LK'...'M;'];
-  imageUrl;
+  imageUrl; // imageUrl=`https://cdn.akamai.steamstatic.com/steam/apps/${this.id}/header.jpg`
   playerHistory; // playerHistory=[{ date: "05.04.22", players: 122 }, { date: "04.04.22", players: 200 }, { date: "03.04.22", players: 150 }]
 
   constructor(steamApp) {
     this.id = steamApp.appid;
     this.name = steamApp.name;
-    this.imgUrl = `https://cdn.akamai.steamstatic.com/steam/apps/${this.id}/header.jpg`
+    this.imageUrl = `https://cdn.akamai.steamstatic.com/steam/apps/${this.id}/header.jpg`
   }
 }
