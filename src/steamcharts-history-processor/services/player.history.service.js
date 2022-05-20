6b513df004113d950915d242426e3ccdf76e2d88
Luka Tarman <luka.tarman@gmail.com>
2022-05-20 17:51:34 +0200

    refactored even more
    # not using players instantiator

--- src/steamcharts-history-processor/services/player.history.service.js
+++ src/steamcharts-history-processor/services/player.history.service.js
@@ -1,4 +1,5 @@
 import { JSDOM } from "jsdom";
+import { Players } from "../../src/models/players.js";
 
 export function parsePlayerHistory(pageHttpDetailsHtml) {
   const dom = new JSDOM(pageHttpDetailsHtml);
@@ -8,10 +9,5 @@ export function parsePlayerHistory(pageHttpDetailsHtml) {
   return myArray.map(entry => entry.firstElementChild)
                 .map(firstElement => firstElement.textContent === "Last 30 Days" ? undefined: firstElement)
                 .filter(element => !!element)
-                .map(element => {
-                    return {
-                        date: new Date(element.textContent),
-                        players: parseFloat(parseFloat(element.nextElementSibling.textContent).toFixed(1)),
-                    }
-                });
+                .map(element => new Players(new Date(), parseFloat(parseFloat(element.nextElementSibling.textContent).toFixed(1))));
 }
