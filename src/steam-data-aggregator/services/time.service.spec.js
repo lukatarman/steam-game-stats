75a831c3458b5268a8d79abb01e25a9bed4771dc
Luka Tarman <luka.tarman@gmail.com>
2022-05-20 08:40:40 +0200

    fixed test bug

--- src/steam-data-aggregator/services/time.service.spec.js
+++ src/steam-data-aggregator/services/time.service.spec.js
@@ -58,7 +58,7 @@ describe("time.service.js", () => {
       });
 
       it("function returns 10800000", () => {
-        expect(result).toBeGreaterThan(10899995);
+        expect(result).toBeGreaterThan(10799995);
       });
     });
   });
