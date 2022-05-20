1c8c226d4fcd511f5ba32b30b878e8d265bd6ed7
Luka Tarman <luka.tarman@gmail.com>
2022-05-20 20:15:36 +0200

    developed constructor

--- src/models/players.js
+++ src/models/players.js
@@ -3,8 +3,8 @@ export class Players {
   players; // players=0..n
 
   constructor(date, players) {
-    this.date = date;
-    this.players = players;
+    this.date = new Date(date);
+    this.players = parseFloat(parseFloat(players).toFixed(1))
   }
 }
 
