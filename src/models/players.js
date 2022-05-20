07edf9db8dfedb918e261bcec35716a498f46cd3
Luka Tarman <luka.tarman@gmail.com>
2022-05-20 17:51:18 +0200

    added constructor

--- src/models/players.js
+++ src/models/players.js
@@ -1,5 +1,10 @@
 export class Players {
   date;    // date=new Date('exact time when player numbers are collected');
   players; // players=0..n
+
+  constructor(date, players) {
+    this.#date = date;
+    this.#players = players;
+  }
 }
 
