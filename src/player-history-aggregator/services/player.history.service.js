d0f1f72055eb0398eada58b37f5ff65cbfdca240
Luka Tarman <luka.tarman@gmail.com>
2022-05-21 08:52:49 +0200

    removed coupling

--- src/player-history-aggregator/services/player.history.service.js
+++ src/player-history-aggregator/services/player.history.service.js
@@ -9,5 +9,5 @@ export function parsePlayerHistory(pageHttpDetailsHtml) {
   return myArray.map(entry => entry.firstElementChild)
                 .map(firstElement => firstElement.textContent === "Last 30 Days" ? undefined : firstElement)
                 .filter(element => !!element)
-                .map(element => new Players(element, element.nextElementSibling));
+                .map(element => new Players(element.textContent, element.nextElementSibling.textContent));
 }
