575beb9c3b1128b55457fcfb02912242086c9b60
Stanislav Jakuschevskij <stanislav.jakuschevskij@at.ibm.com>
2022-05-28 16:39:06 +0200

    added static factory method

--- src/models/game.js
+++ src/models/game.js
@@ -1,8 +1,8 @@
 export class Game {
-  id; // appid=123456
-  name; // name="Metro"
-  image; // imgData=['D%', '7Z', 'HJ', ')I', 'LK'...'M;'];
-  imageUrl; // imageUrl=`https://cdn.akamai.steamstatic.com/steam/apps/${this.id}/header.jpg`
+  id;            // appid=123456
+  name;          // name="Metro"
+  image;         // imgData=['D%', '7Z', 'HJ', ')I', 'LK'...'M;'];
+  imageUrl;      // imageUrl=`https://cdn.akamai.steamstatic.com/steam/apps/${this.id}/header.jpg`
   playerHistory; // playerHistory=[{ date: "05.04.22", players: 122 }, { date: "04.04.22", players: 200 }, { date: "03.04.22", players: 150 }]
   checkedSteamchartsHistory;
 
@@ -12,6 +12,16 @@ export class Game {
     this.imageUrl = `https://cdn.akamai.steamstatic.com/steam/apps/${this.id}/header.jpg`
     this.playerHistory = [];
     this.checkedSteamchartsHistory = false;
+  }
+
+  static fromDbEntry(dbEntry) {
+    const game                     = new Game();
+    game.id                        = dbEntry.id;
+    game.name                      = dbEntry.name;
+    game.imageUrl                  = dbEntry.imageUrl;
+    game.playerHistory             = dbEntry.playerHistory;
+    game.checkedSteamchartsHistory = dbEntry.checkedSteamchartsHistory;
+    return game;
   }
 
   get lastUpdate() {
