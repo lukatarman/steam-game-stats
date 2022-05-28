3b2bd66360ee615fdfb323763b008ba2e50a7b7b
Stanislav Jakuschevskij <stanislav.jakuschevskij@at.ibm.com>
2022-05-28 17:02:09 +0200

    extracted function

--- src/player-history-aggregator/services/player.history.service.js
+++ src/player-history-aggregator/services/player.history.service.js
@@ -1,6 +1,19 @@
 import { JSDOM } from "jsdom";
 import { Players } from "../../models/players.js";
 
+/**
+ * @todo add tests
+ */
+export function addCurrentPlayersFromSteam(players, games) {
+  return games.map((game, i) => {
+    game.playerHistory.push(new Players(players[i]));
+    return game;
+  });
+}
+
+/**
+ * @todo add tests
+ */
 export function addPlayerHistories(pages, games) {
   return games.map((game, i) => {
     if(pages[i] !== "") game.playerHistory = parsePlayerHistory(pages[i]);
