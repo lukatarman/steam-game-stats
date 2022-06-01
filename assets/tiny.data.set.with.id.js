7964dc7b2f110a6d79984f76a726d7d8f8642d4e
Luka Tarman <luka.tarman@gmail.com>
2022-06-01 11:22:21 +0200

    fixed asset stucture to use id
    # instead of appid

--- assets/tiny.data.set.with.id.js
+++ assets/tiny.data.set.with.id.js
@@ -1,118 +1,118 @@
 export const tinyGames = [
   {
-    appid: 1902100,
+    id: 1902100,
     name: "Winter Eggspansion for Eggcelerate!",
   },
   {
-    appid: 1902110,
+    id: 1902110,
     name: "Rising Sun VR",
   },
   {
-    appid: 1902120,
+    id: 1902120,
     name: "Bigfield 2042",
   },
   {
-    appid: 1902130,
+    id: 1902130,
     name: "Oldowan Demo",
   },
   {
-    appid: 1902150,
+    id: 1902150,
     name: "STRANGER",
   },
   {
-    appid: 1902160,
+    id: 1902160,
     name: "Fiasco Restoration and Repair Demo",
   },
   {
-    appid: 1902170,
+    id: 1902170,
     name: "The Asfrixa",
   },
   {
-    appid: 1902190,
+    id: 1902190,
     name: "Project XMAS Demo",
   },
   {
-    appid: 1902200,
+    id: 1902200,
     name: "Scorpion Overdrive",
   },
   {
-    appid: 1902240,
+    id: 1902240,
     name: "Furry Hentai Quest",
   },
   {
-    appid: 1902270,
+    id: 1902270,
     name: "Snow Plow Playtest",
   },
   {
-    appid: 1902290,
+    id: 1902290,
     name: "Run Away SkyDancer",
   },
   {
-    appid: 1902310,
+    id: 1902310,
     name: "The Asfrixa Demo",
   },
   {
-    appid: 1902350,
+    id: 1902350,
     name: "Polar Bear Riding A Ninja",
   },
   {
-    appid: 1902590,
+    id: 1902590,
     name: "Logic World Soundtrack",
   },
   {
-    appid: 1902630,
+    id: 1902630,
     name: "Lighthouse of Madness Playtest",
   },
   {
-    appid: 1902680,
+    id: 1902680,
     name: "Underworld Dreams: The False King Demo",
   },
   {
-    appid: 1902710,
+    id: 1902710,
     name: "奇怪的RPG",
   },
   {
-    appid: 1901610,
+    id: 1901610,
     name: "B-RAIN",
   },
   {
-    appid: 1901700,
+    id: 1901700,
     name: "TRADER LIFE SIMULATOR 2",
   },
   {
-    appid: 1901720,
+    id: 1901720,
     name: "Is it you? Demo",
   },
   {
-    appid: 1901730,
+    id: 1901730,
     name: "Lootbox RPG",
   },
   {
-    appid: 1901740,
+    id: 1901740,
     name: "Mass-Energy Soldier",
   },
   {
-    appid: 1901750,
+    id: 1901750,
     name: "Two Point Hospital: Speedy Recovery",
   },
   {
-    appid: 1901770,
+    id: 1901770,
     name: "Robo Maestro Demo",
   },
   {
-    appid: 1901790,
+    id: 1901790,
     name: "On the Hunt Demo",
   },
   {
-    appid: 1901820,
+    id: 1901820,
     name: "Crsed - Dark Horse Pack",
   },
   {
-    appid: 1901830,
+    id: 1901830,
     name: "SønderDø",
   },
   {
-    appid: 1901870,
+    id: 1901870,
     name: "BadTrip",
   },
 ];
