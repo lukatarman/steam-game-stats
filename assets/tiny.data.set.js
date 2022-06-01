7184054d891ca46077bd657cd39d9823c065ec2a
Luka Tarman <luka.tarman@gmail.com>
2022-06-01 10:55:04 +0200

    added tiny games asset

---
+++ assets/tiny.data.set.js
@@ -0,0 +1,118 @@
+export const tinyGames = [
+  {
+    appid: 1902100,
+    name: "Winter Eggspansion for Eggcelerate!",
+  },
+  {
+    appid: 1902110,
+    name: "Rising Sun VR",
+  },
+  {
+    appid: 1902120,
+    name: "Bigfield 2042",
+  },
+  {
+    appid: 1902130,
+    name: "Oldowan Demo",
+  },
+  {
+    appid: 1902150,
+    name: "STRANGER",
+  },
+  {
+    appid: 1902160,
+    name: "Fiasco Restoration and Repair Demo",
+  },
+  {
+    appid: 1902170,
+    name: "The Asfrixa",
+  },
+  {
+    appid: 1902190,
+    name: "Project XMAS Demo",
+  },
+  {
+    appid: 1902200,
+    name: "Scorpion Overdrive",
+  },
+  {
+    appid: 1902240,
+    name: "Furry Hentai Quest",
+  },
+  {
+    appid: 1902270,
+    name: "Snow Plow Playtest",
+  },
+  {
+    appid: 1902290,
+    name: "Run Away SkyDancer",
+  },
+  {
+    appid: 1902310,
+    name: "The Asfrixa Demo",
+  },
+  {
+    appid: 1902350,
+    name: "Polar Bear Riding A Ninja",
+  },
+  {
+    appid: 1902590,
+    name: "Logic World Soundtrack",
+  },
+  {
+    appid: 1902630,
+    name: "Lighthouse of Madness Playtest",
+  },
+  {
+    appid: 1902680,
+    name: "Underworld Dreams: The False King Demo",
+  },
+  {
+    appid: 1902710,
+    name: "奇怪的RPG",
+  },
+  {
+    appid: 1901610,
+    name: "B-RAIN",
+  },
+  {
+    appid: 1901700,
+    name: "TRADER LIFE SIMULATOR 2",
+  },
+  {
+    appid: 1901720,
+    name: "Is it you? Demo",
+  },
+  {
+    appid: 1901730,
+    name: "Lootbox RPG",
+  },
+  {
+    appid: 1901740,
+    name: "Mass-Energy Soldier",
+  },
+  {
+    appid: 1901750,
+    name: "Two Point Hospital: Speedy Recovery",
+  },
+  {
+    appid: 1901770,
+    name: "Robo Maestro Demo",
+  },
+  {
+    appid: 1901790,
+    name: "On the Hunt Demo",
+  },
+  {
+    appid: 1901820,
+    name: "Crsed - Dark Horse Pack",
+  },
+  {
+    appid: 1901830,
+    name: "SønderDø",
+  },
+  {
+    appid: 1901870,
+    name: "BadTrip",
+  },
+];
