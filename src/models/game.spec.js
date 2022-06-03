2c13499bac1123668fc1488b47c49ee9bff21a0a
Luka Tarman <luka.tarman@gmail.com>
2022-06-03 15:57:44 +0200

    added tests

---
+++ src/models/game.spec.js
@@ -0,0 +1,94 @@
+import { Game } from "./game.js"
+
+describe("game.js", function() {
+  describe("Game", function() {
+    describe(".fromSteamApp", function() {
+      describe("is called with incomplete arguments or no arguments, ", function() {
+
+        it(("an Error is thrown"), function() {
+          expect(Game.fromSteamApp).toThrowError();
+        });
+        });
+
+      describe("is called with appropriate attributes", function() {
+        beforeEach(function() {
+          this.testObject = {
+            id: 123,
+            name: "test game",
+            imageUrl: "test url",
+            playerHistory: [],
+          }
+
+          this.myLittleTest = Game.fromSteamApp(this.testObject);
+        });
+
+        it("the returned value is an instance of Game", function() {
+          expect(this.myLittleTest).toBeInstanceOf(Game);
+        })
+      });
+    });
+
+    describe(".fromDbEntry", function() {
+      describe("is called with incomplete arguments or no arguments, ", function() {
+        it(("an Error is thrown"), function() {
+          expect(Game.fromDbEntry).toThrowError();
+        });
+        });
+
+      describe("is called with appropriate attributes", function() {
+        beforeEach(function() {
+          this.testObject = {
+            id: 123,
+            name: "test game",
+            imageUrl: "test url",
+            playerHistory: [],
+          }
+
+          this.myLittleTest = Game.fromDbEntry(this.testObject);
+        });
+
+        it("the returned value is an instance of Game", function() {
+          expect(this.myLittleTest).toBeInstanceOf(Game);
+        })
+      });
+    });
+
+    describe("class is instantiated,", () => {
+      describe("with object whose playerHistory property has an empty array", () => {
+        beforeEach(function () {
+          this.testObject = {
+            id: 123,
+            name: "test game",
+            imageUrl: "test url",
+            playerHistory: [],
+          }
+
+          this.testClass = Game.fromDbEntry(this.testObject);
+
+          this.result = this.testClass.lastUpdate;
+        });
+
+        it("it returns 'undefined'", function() {
+          expect(this.result).toBeUndefined();
+        });
+      });
+
+      describe("with object whose playerHistory property has entries", () => {
+        beforeEach(function () {
+          this.testObject = {
+            id: 123,
+            name: "test game",
+            imageUrl: "test url",
+            playerHistory: [{ date: "21 September 1989" }],
+          }
+
+          this.testClass = Game.fromDbEntry(this.testObject);
+        });
+
+        it("it returns 'undefined'", function() {
+          expect(this.testClass.lastUpdate).toBe("21 September 1989");
+        });
+      });
+    });
+  });
+});
