62a2affbd2bdd51014f420f0fa5288f943ed1306
Luka Tarman <luka.tarman@gmail.com>
2022-06-07 09:17:27 +0200

    added instantiation of fastify
    # added reply
    # added listen on port 3000

--- src/infrastructure/web.server.js
+++ src/infrastructure/web.server.js
@@ -1,10 +1,25 @@
-export class WebServer{
-  constructor() {
+import Fastify from "fastify";
 
+export class WebServer{
+  #server;
+
+  constructor(dbClient) {
+    this.#server = Fastify({
+      logger: true,
+    });
+    
+    this.#server.get('/', function (request, reply) {
+      reply.send({ hello: 'world' })
+    })
   }
 
   start() {
-
+    this.#server.listen(3000, function (err, address) {
+      if (err) {
+        this.#server.log.error(err)
+        process.exit(1)
+      }
+    })
   }
 
   stop() {
