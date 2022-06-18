1af7721ce465f6c2250740205236b3954b310cee
Luka Tarman <luka.tarman@gmail.com>
2022-06-18 18:08:08 +0200

    auto formatted

--- src/models/players.js
+++ src/models/players.js
@@ -2,7 +2,6 @@ export class Players {
   date;
   players;
 
-
   constructor(playersAsString, dateAsString = "") {
     this.date = dateAsString === "" ? new Date() : new Date(dateAsString);
     /**
