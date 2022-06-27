a975d2a80a334c08c29d6ecf1491e4040e019313
Luka Tarman <luka.tarman@gmail.com>
2022-06-27 12:34:22 +0200

    removed unused function

--- src/features/game-identifier/services/game.service.js
+++ src/features/game-identifier/services/game.service.js
@@ -1,24 +1,6 @@
 import { JSDOM } from "jsdom";
 import { Game } from "../../../models/game.js";
 
-export function filterSteamAppsByName(steamApps) {
-  return steamApps.filter((steamApp) => doesNotEndWithDlcOrSoundtrack(steamApp));
-
-  function doesNotEndWithDlcOrSoundtrack(steamApp) {
-    const lowercaseGameName = steamApp.name.toLowerCase();
-    const endsWithDlc = /dlc$/;
-    const endsWithSoundtrack = /soundtrack$/;
-
-    if (
-      !lowercaseGameName.match(endsWithDlc) &&
-      !lowercaseGameName.match(endsWithSoundtrack)
-    )
-      return true;
-
-    return false;
-  }
-}
-
 export function steamAppIsGame(httpDetailsPage) {
   const dom = new JSDOM(httpDetailsPage);
   const breadcrumbElement = dom.window.document.querySelector(".blockbg");
