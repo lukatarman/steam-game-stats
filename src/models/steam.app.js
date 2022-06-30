e2f3dcacfea674bcd27527c9e9a955d762d1f37a
Luka Tarman <luka.tarman@gmail.com>
2022-06-30 18:02:38 +0200

    created SteamApp class
    # added factory functions
    # moved diff function from service to SteamApp class
    
    Co-authored-by: Stanislav Jakuschevskij <twoGiants@users.noreply.github.com>

--- src/models/steam.app.js
+++ src/models/steam.app.js
@@ -1,1 +1,36 @@
-export class SteamApp {}
+export class SteamApp {
+  appid;
+  name;
+  identified;
+  triedVia;
+
+  static manyFromSteamApi(apps) {
+    return apps.map((app) => SteamApp.oneFromSteamApi(app));
+  }
+
+  // prettier-ignore
+  static oneFromSteamApi(data) {
+    const steamApp      = new SteamApp();
+    steamApp.appid      = data.appid;
+    steamApp.name       = data.name;
+    steamApp.identified = false;
+    steamApp.triedVia   = [];
+    return steamApp;
+  }
+
+  // prettier-ignore
+  static fromDbEntry(dbEntry) {
+    const steamApp      = new SteamApp();
+    steamApp.appid      = dbEntry.appid;
+    steamApp.name       = dbEntry.name;
+    steamApp.identified = dbEntry.identified;
+    steamApp.triedVia   = dbEntry.triedVia;
+    return steamApp;
+  }
+
+  static diff(steamAppsSource, steamAppsTarget) {
+    const targetAppIds = steamAppsTarget.map((app) => app.appid);
+
+    return steamAppsSource.filter((app) => !targetAppIds.includes(app.appid));
+  }
+}
