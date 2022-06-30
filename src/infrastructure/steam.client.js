bcc6340f39674f707d8eb3a3cae3c178d75a1213
Luka Tarman <luka.tarman@gmail.com>
2022-06-30 19:11:16 +0200

    added instantiation of SteamApp
    # added import

--- src/infrastructure/steam.client.js
+++ src/infrastructure/steam.client.js
@@ -1,3 +1,5 @@
+import { SteamApp } from "../models/steam.app.js";
+
 export class SteamClient {
   #httpClient;
 
@@ -8,8 +10,9 @@ export class SteamClient {
   async getAppList() {
     const url = "https://api.steampowered.com/ISteamApps/GetAppList/v2";
     const options = { params: { key: "79E04F52C6B5AD21266624C05CC12E42" } };
+    const response = await this.#httpClient.get(url, options).data.applist.apps;
 
-    return (await this.#httpClient.get(url, options)).data.applist.apps;
+    return SteamApp.oneFromSteamApi(response);
   }
 
   async getCurrentPlayers(game) {
