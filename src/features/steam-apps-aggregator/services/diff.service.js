5b943590012f24a08beddb1707531b0be86f965b
Luka Tarman <luka.tarman@gmail.com>
2022-07-01 16:25:00 +0200

    removed unusued services folder

--- src/features/steam-apps-aggregator/services/diff.service.js
+++
@@ -1,5 +0,0 @@
-export function diff(steamAppsApi, steamAppsDb) {
-  const steamAppsDbIds = steamAppsDb.map((app) => app.appid);
-
-  return steamAppsApi.filter((app) => !steamAppsDbIds.includes(app.appid));
-}
