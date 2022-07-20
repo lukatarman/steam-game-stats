9e8fc056f108cf9d15b0731962b523fdb5a2b813
Luka Tarman <luka.tarman@gmail.com>
2022-07-20 19:14:30 +0200

    refactored discover function

--- src/features/game-identifier/services/game.service.js
+++ src/features/game-identifier/services/game.service.js
@@ -26,8 +26,26 @@ export function discoverGamesFromSteamWeb(steamApps, htmlDetailsPages) {
   for (let i = 0; i < steamApps.length; i++) {
     steamAppIsGame(htmlDetailsPages[i])
       ? games.push(Game.fromSteamApp(steamApps[i]))
-      : unidentifiedSteamApps.push(SteamApp.oneFromSteamApi(steamApps[i], ["steamWeb"]));
+      : unidentifiedSteamApps.push(steamApps[i]);
   }
 
   return [games, unidentifiedSteamApps];
+}
+
+export function XXXdiscoverGamesFromSteamWeb(steamApps, htmlDetailsPages) {
+  const games = htmlDetailsPages
+    .filter((page) => steamAppIsGame(page))
+    .map((_, i) => Game.fromSteamApp(steamApps[i]));
+
+  const updatedSteamApps = htmlDetailsPages.map((page, i) => {
+    const copy = steamApps[i].copy();
+
+    copy.triedViaSteamWeb();
+
+    if (steamAppIsGame(page[i])) copy.identify();
+
+    return copy;
+  });
+
+  return [games, updatedSteamApps];
 }
