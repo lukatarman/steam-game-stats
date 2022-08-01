0a0e8b1690c495b1c727b32c7d30458974de94e9
Luka Tarman <luka.tarman@gmail.com>
2022-08-01 14:12:25 +0200

    added triedViaStemWeb method test

--- src/models/steam.app.spec.js
+++ src/models/steam.app.spec.js
@@ -29,6 +29,35 @@ describe("SteamApp", function () {
       });
     });
 
+    describe(".triedViaSteamWeb", function () {
+      describe("pushes 'steamWeb' into the triedVia property. When this is done,", function () {
+        beforeEach(function () {
+          this.app = {
+            name: "Castlevania",
+            appid: 1,
+          };
+
+          this.instantiatedApp = SteamApp.oneFromSteamApi(this.app);
+
+          this.result = this.instantiatedApp.copy();
+
+          this.result.triedViaSteamWeb();
+        });
+
+        it("the copy is an instance of SteamApp", function () {
+          expect(this.result).toBeInstanceOf(SteamApp);
+        });
+
+        it("the triedVia property contains a single array", function () {
+          expect(this.result.triedVia.length).toBe(1);
+        });
+
+        it("the triedVia property array value is 'steamWeb'", function () {
+          expect(this.result.triedVia[0]).toBe("steamWeb");
+        });
+      });
+    });
+
     describe(".manyFromSteamApi returns an array of SteamApp instances.", function () {
       describe("When two apps are passed into it,", function () {
         beforeEach(function () {
