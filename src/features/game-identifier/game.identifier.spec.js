129cad4acad5a45296be7a5ff6f3ab0db51c85c1
Luka Tarman <luka.tarman@gmail.com>
2022-08-01 16:38:50 +0200

    added first test case

---
+++ src/features/game-identifier/game.identifier.spec.js
@@ -0,0 +1,37 @@
+import { GameIdentifier } from "./game.identifier.js";
+
+fdescribe("game.identifier.js", function () {
+  describe(".tryViaSteamWeb", function () {
+    describe("gets zero steamApps from the database and stops. So, ", function () {
+      beforeEach(function () {
+        this.databaseClientMock = jasmine.createSpyObj("databaseClient", {
+          getSteamWebUntriedFilteredSteamApps: Promise.resolve([]),
+        });
+
+        this.steamClientMock = jasmine.createSpyObj("steamClient", {
+          getSteamAppHtmlDetailsPage: Promise.resolve(undefined),
+        });
+
+        this.identifier = new GameIdentifier(
+          this.steamClientMock,
+          this.databaseClientMock,
+          {
+            batchSize: 1,
+          },
+        );
+
+        this.identifier.tryViaSteamWeb();
+      });
+
+      it("getSteamWebUntriedFilteredSteamApps was called once", function () {
+        expect(
+          this.databaseClientMock.getSteamWebUntriedFilteredSteamApps,
+        ).toHaveBeenCalledTimes(1);
+      });
+
+      it("getSteamAppHtmlDetailsPage was not called", function () {
+        expect(this.steamClientMock.getSteamAppHtmlDetailsPage).toHaveBeenCalledTimes(0);
+      });
+    });
+  });
+});
