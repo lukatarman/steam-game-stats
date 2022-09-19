7980794246c66843b1929ef03b77226d59a4bded
Luka Tarman <luka.tarman@gmail.com>
2022-09-19 11:09:33 +0200

    added basic stlying

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,15 @@
+import Search from "./Search.js";
+import "./app.css";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="container main-card centered">
+      <h1 className="title">Steam Game Stats</h1>
+      <div>
+        <Search />
+      </div>
+    </div>
+  );
 };
 
 export default App;
