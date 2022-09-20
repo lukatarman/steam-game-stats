b79960eea4994e72cb7c17ddf756fb8ef2adab4f
Luka Tarman <luka.tarman@gmail.com>
2022-09-20 11:32:49 +0200

    added navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,11 @@
 import Search from "./Search.js";
+import Navbar from "./Navbar.js";
 import "./app.css";
 
 const App = () => {
   return (
-    <div className="ui container">
-      <div className=" main-card centered">
-        <h1 className="title">Steam Game Stats</h1>
-        <div style={{ backgroundColor: "black" }}>
-          <Search />
-        </div>
-      </div>
+    <div>
+      <Navbar />
     </div>
   );
 };
