4d61793e11eaa96e0c1bd57cc3f585bcef449609
Luka Tarman <luka.tarman@gmail.com>
2022-09-20 12:10:16 +0200

    adjusted navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,11 +8,24 @@ const Navbar = () => {
       role="navigation"
       aria-label="main navigation"
     >
-      <div id="navbarBasicExample" class="navbar-menu">
-        <div class="navbar-start">
-          <a class="navbar-item">Steam Game Stats</a>
-        </div>
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://bulma.io">
+          <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+        </a>
 
+        <a
+          role="button"
+          class="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div id="navbarBasicExample" class="navbar-menu is-active">
         <div class="navbar-end">
           <div class="navbar-item">Search</div>
         </div>
