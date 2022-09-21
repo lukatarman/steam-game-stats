9db15f278ffff65cc19aeb443f5b138f5c894014
Luka Tarman <luka.tarman@gmail.com>
2022-09-21 15:30:22 +0200

    removed old bulma HTML/CSS

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,36 +2,7 @@ import React, { useRef, useEffect } from "react";
 import "./navbar.css";
 
 const Navbar = () => {
-  return (
-    <nav
-      class="navbar is-fixed-top is-black"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div class="navbar-brand">
-        <a class="navbar-item" href="https://bulma.io">
-          <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
-        </a>
-
-        <a
-          role="button"
-          class="navbar-burger"
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbarBasicExample"
-        >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-      <div id="navbarBasicExample" class="navbar-menu is-active">
-        <div class="navbar-end">
-          <div class="navbar-item">Search</div>
-        </div>
-      </div>
-    </nav>
-  );
+  return <div>Navbar</div>;
 };
 
 export default Navbar;
