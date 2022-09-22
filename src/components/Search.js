092d61a20b28ecc750a4d9769c35f52e525fc9ff
Luka Tarman <luka.tarman@gmail.com>
2022-09-22 15:47:59 +0200

    added form for search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { Form } from "react-bootstrap";
 import "./search.css";
 
 const Search = () => {
   // add fontawesome icon to whole search input bar
   return (
-    <div>
-      <FontAwesomeIcon icon={faMagnifyingGlass} size="" />
-    </div>
+    <Form>
+      <Form.Control type="search" placeholder="Enter email" />
+    </Form>
   );
 };
 
