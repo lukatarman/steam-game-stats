1d9842f06b7f6bc40a717dee2bed778c34c5ef09
Luka Tarman <luka.tarman@gmail.com>
2022-09-22 13:44:56 +0200

    removed fontawesome icon

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,8 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import "./search.css";
 
 const Search = () => {
-  return (
-    <div>
-      <FontAwesomeIcon icon={faMagnifyingGlass} size="lg" />
-    </div>
-  );
+  return <div>Search</div>;
 };
 
 export default Search;
