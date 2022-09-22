b699d82dfb771e7a6b2d7ad5a719a717a9658e09
Luka Tarman <luka.tarman@gmail.com>
2022-09-22 13:47:45 +0200

    returned font awesome icon
    # added todo

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import "./search.css";
 
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div>
+      // add fontawesome icon to whole search input bar
+      <FontAwesomeIcon icon={faMagnifyingGlass} size="" />
+    </div>
+  );
 };
 
 export default Search;
