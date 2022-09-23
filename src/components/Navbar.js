ce81938d52fcb8b27741a7657124aa29b3e3db8e
Luka Tarman <luka.tarman@gmail.com>
2022-09-23 11:25:19 +0200

    removed unused argument and imports
    # removed unused Container

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,22 +6,20 @@ import "./navbar.css";
 
 const SgsNavbar = () => {
   return (
-    <Container>
-      <Navbar fixed="top" bg="light" expand="lg">
-        <Container fluid>
-          <Navbar.Brand href="#">Steam Game Stats</Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-          </Form>
-        </Container>
-      </Navbar>
-    </Container>
+    <Navbar fixed="top" bg="light" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="#">Steam Game Stats</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Form className="d-flex">
+          <Form.Control
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+          />
+        </Form>
+      </Container>
+    </Navbar>
   );
 };
 
