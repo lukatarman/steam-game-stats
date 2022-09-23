fabf725a7a9247738cb3513712f6f3e11442b139
Luka Tarman <luka.tarman@gmail.com>
2022-09-23 15:06:50 +0200

    changed to contstructor enums in object

--- src/features/game-identifier/services/game.service.js
+++ src/features/game-identifier/services/game.service.js
@@ -3,23 +3,22 @@ import { Game } from "../../../models/game.js";
 import { SteamApp } from "../../../models/steam.app.js";
 
 export function getSteamAppType(httpDetailsPage) {
-  const results = SteamApp.createEnum(["unknown", "game", "downloadableContent"]);
-
   const dom = new JSDOM(httpDetailsPage);
   const breadcrumbElement = dom.window.document.querySelector(".blockbg");
 
-  if (!breadcrumbElement) return results.unknown;
+  if (!breadcrumbElement) return SteamApp.type.unknown;
 
   const breadcrumbText = breadcrumbElement.children[0].textContent;
 
   if (breadcrumbText !== "All Software" && breadcrumbText !== "All Games")
-    return results.unknown;
+    return SteamApp.type.unknown;
 
   for (let child of breadcrumbElement.children) {
-    if (child.textContent === "Downloadable Content") return results.downloadableContent;
+    if (child.textContent === "Downloadable Content")
+      return SteamApp.type.downloadableContent;
   }
 
-  return results.game;
+  return SteamApp.type.game;
 }
 
 export function discoverGamesFromSteamWeb(steamApps, htmlDetailsPages) {
