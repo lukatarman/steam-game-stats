14f33aa8260d990fb37d48a40c1d3067c006a822
Luka Tarman <luka.tarman@gmail.com>
2022-09-23 11:22:49 +0200

    added navbar with search
    # Added fixed top position

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,30 @@
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Button from "react-bootstrap/Button";
 
 import "./navbar.css";
 
 const SgsNavbar = ({ includeSearch }) => {
   return (
-    <Navbar>
-      <Container>
-        <Navbar.Collapse className="justify-content-center">
-          <Navbar.Text className="navbar-item">Steam Game Stats</Navbar.Text>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <Container>
+      <Navbar fixed="top" bg="light" expand="lg">
+        <Container fluid>
+          <Navbar.Brand href="#">Steam Game Stats</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+          </Form>
+        </Container>
+      </Navbar>
+    </Container>
   );
 };
 
