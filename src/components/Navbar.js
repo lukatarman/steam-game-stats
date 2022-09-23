4081f57d2c063d651412683a49403cb776413a89
Luka Tarman <luka.tarman@gmail.com>
2022-09-23 11:16:51 +0200

    removed search component and import

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,4 @@
 import Navbar from "react-bootstrap/Navbar";
-import Search from "./Search.js";
 import Container from "react-bootstrap/Container";
 
 import "./navbar.css";
@@ -10,7 +9,6 @@ const SgsNavbar = ({ includeSearch }) => {
       <Container>
         <Navbar.Collapse className="justify-content-center">
           <Navbar.Text className="navbar-item">Steam Game Stats</Navbar.Text>
-          <Navbar.Text>{includeSearch && <Search />}</Navbar.Text>
         </Navbar.Collapse>
       </Container>
     </Navbar>
