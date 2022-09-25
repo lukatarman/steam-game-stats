924a021bfff75bada8bdd0b39ad8aa1babc20da4
Luka Tarman <luka.tarman@gmail.com>
2022-09-25 13:28:16 +0200

    added twelve hours to date
    # added 12 hours to playerHistory date from steamcharts
    # removed todo
    # added method for calculation

--- src/models/players.js
+++ src/models/players.js
@@ -3,19 +3,15 @@ export class Players {
   players;
 
   constructor(playersAsString, dateAsString = "") {
-    //todo: when mongoDb stores dates into its databases, it automatically transforms them into UTC format.
-    // https://www.mongodb.com/docs/v3.2/tutorial/model-time-data/
-    //Think about changing steamcharts time to noon each day instead of midnight. Timezone differences wont mess up day
-    // as for current time just leave it as it is, it most likely ownt mess up anything
-    // add 12 hours to date, if date isnt empty string
+    this.date = dateAsString === "" ? new Date() : this.#addTwelveHours(dateAsString);
 
-    const currentTimeUTC = new Date();
-    const timeUTC = new Date(dateAsString);
-
-    this.date = dateAsString === "" ? currentTimeUTC : timeUTC;
-    /**
-     * @TODO - https://github.com/lukatarman/steam-game-stats/issues/52
-     */
     this.players = parseFloat(parseFloat(playersAsString).toFixed(1));
+  }
+
+  #addTwelveHours(date) {
+    const dateInMs = Date.parse(date);
+    const twelveHoursInMs = 12 * 60 * 60 * 1000;
+
+    return new Date(dateInMs + twelveHoursInMs);
   }
 }
