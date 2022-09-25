cba68880278ef31249162c38991b486e3e940b2d
Luka Tarman <luka.tarman@gmail.com>
2022-09-25 14:18:27 +0200

    added axios call to our endpoint

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
+import axios from "axios";
+import { useEffect } from "react";
 import SgsNavbar from "./SgsNavbar.js";
 import SgsTable from "./SgsTable.js";
 import "./app.css";
 
 const App = () => {
+  useEffect(() => {
+    const response = axios.get("http://localhost:3000/getTopTenGames");
+    console.log(response);
+  });
+
   return (
     <div>
       <SgsNavbar />
