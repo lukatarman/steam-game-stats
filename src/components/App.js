a2e5c09cfd27bb7b2706694c83dd3404f7b2f225
Luka Tarman <luka.tarman@gmail.com>
2022-09-26 22:30:55 +0200

    added todos

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,10 @@ import SgsNavbar from "./SgsNavbar.js";
 import SgsTable from "./SgsTable.js";
 import "./app.css";
 
+//todo
+// https://www.fastify.io/docs/latest/Guides/Getting-Started/
+// https://www.fastify.io/docs/latest/Reference/Routes/
+
 const App = () => {
   useEffect(() => {
     const response = axios.get("http://localhost:3000/getTopTenGames");
