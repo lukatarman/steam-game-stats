2e7b74cf001a67eec87db0e4bbfa08df8f9dc51a
Luka Tarman <luka.tarman@gmail.com>
2022-10-03 15:29:38 +0200

    renamed searchResultsTable index file

---
+++ src/components/SearchResultsTable/search.results.table.view.js
@@ -0,0 +1,21 @@
+import Table from "react-bootstrap/Table";
+import businessLogic from "./search.results.table.behavoir.js";
+
+function SearchResultsTable({ tableData, tableOptions }) {
+  const renderResults = businessLogic(tableData);
+
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>{tableOptions.firstCol}</th>
+          <th>{tableOptions.secondCol}</th>
+        </tr>
+      </thead>
+      <tbody>{renderResults}</tbody>
+    </Table>
+  );
+}
+
+export default SearchResultsTable;
