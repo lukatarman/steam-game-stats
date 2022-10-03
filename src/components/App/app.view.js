f4d9db17b4f27e473911c2265a6a12210d9418a8
Luka Tarman <luka.tarman@gmail.com>
2022-10-03 15:30:00 +0200

    adjusted imports

--- src/components/App/app.view.js
+++ src/components/App/app.view.js
@@ -1,6 +1,6 @@
 import AppBehavior from "./app.behavoir.js";
 import SearchBar from "../SearchBar";
-import SearchResultsTable from "../SearchResultsTable";
+import SearchResultsTable from "../SearchResultsTable/search.results.table.view";
 
 const AppView = () => {
   const [topTenGames, tableOptions] = AppBehavior();
