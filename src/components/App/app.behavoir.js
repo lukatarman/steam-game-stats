e4dcd50f4a0cc16570eb6677cd54c6a011e92568
Luka Tarman <luka.tarman@gmail.com>
2022-10-03 15:26:33 +0200

    renamed App business logic file

---
+++ src/components/App/app.behavoir.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import { getTopTenGamesData } from "../../adapters/axios";
+
+const AppBehavior = () => {
+  const [topTenGames, setTopTenGames] = useState([]);
+
+  const tableOptions = {
+    firstCol: "Game Name",
+    secondCol: "Current Players",
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getTopTenGamesData();
+      setTopTenGames(data);
+    };
+
+    fetchData();
+  }, []);
+
+  return [topTenGames, tableOptions];
+};
+
+export default AppBehavior;
