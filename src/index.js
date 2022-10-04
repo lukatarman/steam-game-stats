6e47970d1713f97abbf0155948aa282f83980b3d
Luka Tarman <luka.tarman@gmail.com>
2022-10-04 14:09:50 +0200

    removed view

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AppView from "./components/App/app.view.js";
+import App from "./components/App/app.view.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppView />);
+root.render(<App />);
