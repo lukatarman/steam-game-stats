b4eac936581b078e89183ae0822466eab16fbf5c
Luka Tarman <luka.tarman@gmail.com>
2022-10-04 16:13:42 +0200

    imported behavior and its variables

--- src/components/SearchBar/search.bar.view.js
+++ src/components/SearchBar/search.bar.view.js
@@ -1,43 +1,11 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import { Link } from "react-router-dom";
+import SearchBarBehavior from "./search.bar.behavior.js";
 
-const SgsNavbar = () => {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [searchResponse, setSearchResponse] = useState([]);
-  const [isOpen, setIsOpen] = useState(false);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await axios.get(`http://localhost:3000/games?q=${searchTerm}`);
-      setSearchResponse(response.data);
-    };
-
-    if (searchTerm) {
-      fetchData();
-      setIsOpen(true);
-    }
-
-    if (!searchTerm) setIsOpen(false);
-  }, [searchTerm]);
-
-  const onInputChange = (e) => {
-    setSearchTerm(e.target.value);
-  };
-
-  const searchList = searchResponse.map((result) => {
-    return (
-      <div key={result.id}>
-        <img src={result.imageUrl}></img>
-        <Link to={"/games/" + result.id} style={{ textDecoration: "none" }}>
-          <strong>Game name</strong>: {result.name}
-        </Link>
-      </div>
-    );
-  });
+const SearchBar = () => {
+  const [searchTerm, isOpen, searchResultsList, onInputChange] = SearchBarBehavior();
 
   return (
     <div>
@@ -59,7 +27,7 @@ const SgsNavbar = () => {
           </Form>
         </Container>
       </Navbar>
-      <div>{isOpen ? searchList : null}</div>
+      <div>{isOpen ? searchResultsList : null}</div>
     </div>
   );
 };
