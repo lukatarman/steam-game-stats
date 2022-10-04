b5610d77ebde877f3350045f58244fd470a09a43
Luka Tarman <luka.tarman@gmail.com>
2022-10-04 16:13:02 +0200

    added function to http client

--- src/adapters/http-client/http.client.adapter.js
+++ src/adapters/http-client/http.client.adapter.js
@@ -4,3 +4,8 @@ export async function getTopTenGamesData() {
   const response = await axios.get("http://localhost:3000/games?sort=desc&limit=10");
   return response.data;
 }
+
+export async function getSearchResults(searchTerm) {
+  const response = await axios.get(`http://localhost:3000/games?q=${searchTerm}`);
+  return response.data;
+}
