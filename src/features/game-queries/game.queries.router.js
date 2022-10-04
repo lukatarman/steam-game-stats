35179566e409f822ca086a7568ceab6faf9f2f5d
Luka Tarman <luka.tarman@gmail.com>
2022-10-04 11:52:41 +0200

    changed search endpoint to games query

--- src/features/game-queries/game.queries.router.js
+++ src/features/game-queries/game.queries.router.js
@@ -9,6 +9,9 @@ export class GameQueriesRouter {
     server.get("/games", async (request, reply) => {
       const limit = request.query.limit ? request.query.limit : 10;
 
+      if (request.query.q)
+        return await this.#controller.getGamesBySearchTerm(request.query.q);
+
       if (request.query.sort) return await this.#controller.getTopXgames(parseInt(limit));
 
       return await this.#controller.getAllGames();
@@ -18,12 +21,6 @@ export class GameQueriesRouter {
       const id = parseInt(request.params.id);
 
       return await this.#controller.getOneGameById(id);
-    });
-
-    server.get("/search", async (request, reply) => {
-      const term = request.query.q;
-
-      return await this.#controller.getGamesBySearchTerm(term);
     });
   };
 }
