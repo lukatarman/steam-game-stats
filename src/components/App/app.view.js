2b92ccf694c3f07db97f1474b97b627515d85258
Luka Tarman <luka.tarman@gmail.com>
2022-10-10 12:03:14 +0200

    remove link import

--- src/components/App/app.view.js
+++ src/components/App/app.view.js
@@ -1,4 +1,4 @@
-import { Switch, Route, Link } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import AppBehavior from "./app.behavior.js";
 import GameDetails from "../GameDetails/game.details.view.js";
 import SearchBar from "../SearchBar/search.bar.view.js";
