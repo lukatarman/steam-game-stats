bd6873cb14d2aa6e4b3a72adced15a0908f51fe2
Luka Tarman <luka.tarman@gmail.com>
2022-10-18 10:41:21 +0200

    added Container to top games table
    # added import

--- src/components/TopGamesTable/top.games.table.view.js
+++ src/components/TopGamesTable/top.games.table.view.js
@@ -1,20 +1,23 @@
 import Table from "react-bootstrap/Table";
 import TopGamesTableBehavior from "./top.games.table.behavior.js";
+import Container from "react-bootstrap/Container";
 
 function TopGamesTable({ tableData, tableOptions }) {
   const renderResults = TopGamesTableBehavior(tableData);
 
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>{tableOptions.firstCol}</th>
-          <th>{tableOptions.secondCol}</th>
-        </tr>
-      </thead>
-      <tbody>{renderResults}</tbody>
-    </Table>
+    <Container fluid="lg">
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>{tableOptions.firstCol}</th>
+            <th>{tableOptions.secondCol}</th>
+          </tr>
+        </thead>
+        <tbody>{renderResults}</tbody>
+      </Table>
+    </Container>
   );
 }
 
