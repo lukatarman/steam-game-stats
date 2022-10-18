dd109fda5fc8f2ddb1c1ea5afc40d93a492cdb26
Luka Tarman <luka.tarman@gmail.com>
2022-10-18 11:01:39 +0200

    added inline styling

--- src/components/TopGamesTable/top.games.table.view.js
+++ src/components/TopGamesTable/top.games.table.view.js
@@ -6,18 +6,23 @@ function TopGamesTable({ tableData, tableOptions }) {
   const renderResults = TopGamesTableBehavior(tableData);
 
   return (
-    <Container fluid="lg">
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>{tableOptions.firstCol}</th>
-            <th>{tableOptions.secondCol}</th>
-          </tr>
-        </thead>
-        <tbody>{renderResults}</tbody>
-      </Table>
-    </Container>
+    <div style={{ marginBottom: "2000px" }}>
+      <Container fluid="lg">
+        <div style={{ backgroundColor: "#fafafa", padding: "15px 15px" }}>
+          <h3 style={{ marginBottom: "15px" }}>Top Ten Games</h3>
+          <Table striped bordered hover style={{ padding: "0px 0px" }}>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>{tableOptions.firstCol}</th>
+                <th>{tableOptions.secondCol}</th>
+              </tr>
+            </thead>
+            <tbody>{renderResults}</tbody>
+          </Table>
+        </div>
+      </Container>
+    </div>
   );
 }
 
