95f6f92a45365e907a77dc5e39cb040ddf1ae8da
Luka Tarman <luka.tarman@gmail.com>
2022-10-19 12:37:16 +0200

    added clickable links to top ten table

--- src/components/TopGamesTable/top.games.table.behavior.js
+++ src/components/TopGamesTable/top.games.table.behavior.js
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
+
 const TopGamesTableBehavior = (tableData) => {
   const renderResults = tableData.map((data, index) => {
     return (
       <tr key={data.id}>
         <td>{index + 1}</td>
-        <td>{data.name}</td>
+        <td>
+          <Link to={"/game/" + data.id}>{data.name}</Link>
+        </td>
         <td>{data.currentPlayers}</td>
       </tr>
     );
