cb33825598d4d990e8ff26da0780a5524706a388
Luka Tarman <luka.tarman@gmail.com>
2022-10-26 23:43:44 +0200

    added Container

--- src/components/GameDetails/game.details.view.js
+++ src/components/GameDetails/game.details.view.js
@@ -7,19 +7,21 @@ const GameDetails = () => {
 
   return (
     <div>
-      <div>
-        <img src={gameData.imageUrl} alt="Not found" />
-        <div>{gameData.name}</div>
-      </div>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Month</th>
-            <th>Average Players</th>
-          </tr>
-        </thead>
-        <tbody>{tableContent}</tbody>
-      </Table>
+      <Container>
+        <div>
+          <img src={gameData.imageUrl} alt="Not found" />
+          <div>{gameData.name}</div>
+        </div>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Month</th>
+              <th>Average Players</th>
+            </tr>
+          </thead>
+          <tbody>{tableContent}</tbody>
+        </Table>
+      </Container>
     </div>
   );
 };
