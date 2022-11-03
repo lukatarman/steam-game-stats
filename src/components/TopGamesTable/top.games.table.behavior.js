a95d406170b3ddcd72cb1f07f6aa7a84eebdb0e7
Luka Tarman <luka.tarman@gmail.com>
2022-11-03 15:02:44 +0100

    made thousands separation clearer

--- src/components/TopGamesTable/top.games.table.behavior.js
+++ src/components/TopGamesTable/top.games.table.behavior.js
@@ -2,13 +2,14 @@ import { Link } from "react-router-dom";
 
 const TopGamesTableBehavior = (tableData) => {
   const renderResults = tableData.map((data, index) => {
+    const currentPlayersThousandsSeparated = data.currentPlayers.toLocaleString("en-US");
     return (
       <tr key={data.id}>
         <td>{index + 1}</td>
         <td>
           <Link to={"/game/" + data.id}>{data.name}</Link>
         </td>
-        <td>{data.currentPlayers.toLocaleString("en-US")}</td>
+        <td>{currentPlayersThousandsSeparated}</td>
       </tr>
     );
   });
