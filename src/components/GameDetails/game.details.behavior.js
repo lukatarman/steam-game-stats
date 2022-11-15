eb963bba8aa3d1d6ea35676b5199b4d52d96697c
Luka Tarman <luka.tarman@gmail.com>
2022-11-15 22:43:45 +0100

    added info to log

--- src/components/GameDetails/game.details.behavior.js
+++ src/components/GameDetails/game.details.behavior.js
@@ -13,6 +13,7 @@ const GameDetailsBehavior = () => {
     const fetchData = async () => {
       const response = await getGameById(gameId);
       setGameData(response);
+      console.log("Response is:");
       console.log(response);
     };
 
