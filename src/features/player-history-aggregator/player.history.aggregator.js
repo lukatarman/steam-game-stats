8dde082721532e4f840fb055797b1aa55c0390cc
Luka Tarman <luka.tarman@gmail.com>
2022-11-17 16:02:08 +0100

    added todo steps

--- src/features/player-history-aggregator/player.history.aggregator.js
+++ src/features/player-history-aggregator/player.history.aggregator.js
@@ -6,6 +6,9 @@ import { delay } from "../../utils/time.utils.js";
 import { HistoryCheck } from "../../models/history.check.js";
 
 //Todo: Move average players calculation to backend
+// remove mock function
+// adjust tests
+// check if everything works
 
 export class PlayerHistoryAggregator {
   #steamClient;
