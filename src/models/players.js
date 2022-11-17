61e9dee6e9c340b594fd4f0aaa62fdd8b7861f2e
Luka Tarman <luka.tarman@gmail.com>
2022-11-17 13:05:55 +0100

    added newMonthlyEntry method

--- src/models/players.js
+++ src/models/players.js
@@ -12,4 +12,13 @@ export class Players {
     averagePlayers  = page.averagePlayers;
     trackedPlayers  = [];
   }
+
+  //prettier-ignore
+  static newMonthlyEntry(date) {
+    const players   = new Players();
+    players.year    = date.year;
+    players.month   = date.month;
+    averagePlayers  = undefined;
+    trackedPlayers  = [];
+  }
 }
