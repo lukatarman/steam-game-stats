cdfe85c03cfe3fb39df643419716ae9f97625e67
Luka Tarman <luka.tarman@gmail.com>
2022-11-18 11:27:40 +0100

    added half of the tests

--- src/features/player-history-aggregator/services/player.history.service.spec.js
+++ src/features/player-history-aggregator/services/player.history.service.spec.js
@@ -1,42 +1,140 @@
 import { parsePlayerHistory } from "./player.history.service.js";
+import { XXXaddCurrentPlayersFromSteam } from "./player.history.service.js";
+import { TrackedPlayers } from "../../../models/tracked.players.js";
+import { Players } from "../../../models/players.js";
 import { eldenRingHttpDetailsSteamcharts } from "../../../../assets/steamcharts-details-pages/elden.ring.multiple.histories.html.details.page.js";
 import { sniperEliteHttpDetailsSteamcharts } from "../../../../assets/steamcharts-details-pages/sniper.elite.just.released.html.details.page.js";
 
-xdescribe("player.history.service.js", () => {
-  describe(".parsePlayerHistory", () => {
-    describe("if the game has multiple histories", () => {
-      let playerHistories;
+fdescribe("player.history.service.js", function () {
+  describe(".XXXaddCurrentPlayersFromSteam function adds the current players to the games object and calculates the average players. So,", function () {
+    describe("if the game's 'playerHistory' array is empty, the first array entry in this.result[0].playerHistory'", function () {
+      beforeEach(function () {
+        this.playersFromSteam = ["32"];
 
-      beforeAll(() => {
-        playerHistories = parsePlayerHistory(eldenRingHttpDetailsSteamcharts);
+        this.games = [
+          {
+            id: 1,
+            name: "Random Game",
+            playerHistory: [],
+          },
+        ];
+
+        this.result = XXXaddCurrentPlayersFromSteam(this.playersFromSteam, this.games);
       });
 
-      it("playerHistories has two entries", () => {
-        expect(playerHistories.length).toBe(2);
+      it("will be an instance of Players", function () {
+        expect(this.result[0].playerHistory[0]).toBeInstanceOf(Players);
       });
-
-      it("the first entries' player amount is 522066.4", () => {
-        expect(playerHistories[0].players).toBe(522066.4);
+      it("will have a property called 'average players', which will equal '32'", function () {
+        expect(this.result[0].playerHistory[0].averagePlayers).toBe(32);
       });
-
-      it("the first entries' month is March", () => {
-        expect(playerHistories[0].date.getMonth()).toBe(2);
-      });
-
-      it("the first entries' year is 2022", () => {
-        expect(playerHistories[0].date.getFullYear()).toBe(2022);
+      it("will have a property called 'tracked players' its first array entry will be an instance of 'TrackedPlayers'", function () {
+        expect(this.result[0].playerHistory[0].trackedPlayers[0]).toBeInstanceOf(
+          TrackedPlayers,
+        );
       });
     });
 
-    describe("if the game has just been released and only has one history", () => {
-      let playerHistories;
+    describe("if the game's 'playerHistory' array includes this month's entry", function () {
+      beforeEach(function () {
+        this.playersFromSteam = ["32"];
 
-      beforeAll(() => {
-        playerHistories = parsePlayerHistory(sniperEliteHttpDetailsSteamcharts);
+        this.games = [
+          {
+            id: 1,
+            name: "Random Game",
+            playerHistory: [
+              {
+                year: new Date().getFullYear(),
+                month: new Date().getMonth(),
+                averagePlayers: 0,
+                trackedPlayers: [new TrackedPlayers("23"), new TrackedPlayers("66")],
+              },
+            ],
+          },
+        ];
+
+        this.result = XXXaddCurrentPlayersFromSteam(this.playersFromSteam, this.games);
       });
 
-      it("it returns an empty array", () => {
-        expect(playerHistories).toEqual([]);
+      it("will have a property called 'average players', which will equal '40.3'", function () {
+        expect(this.result[0].playerHistory[0].averagePlayers).toBe("40.3");
+      });
+      it("will have a property called 'tracked players' its first array entry will be an instance of 'TrackedPlayers'", function () {
+        expect(this.result[0].playerHistory[0].trackedPlayers[0]).toBeInstanceOf(
+          TrackedPlayers,
+        );
+      });
+    });
+
+    //continue here
+    describe("if the game's 'playerHistory' array includes this month's entry", function () {
+      beforeEach(function () {
+        this.playersFromSteam = ["32"];
+
+        this.games = [
+          {
+            id: 1,
+            name: "Random Game",
+            playerHistory: [
+              {
+                year: new Date().getFullYear(),
+                month: new Date().getMonth(),
+                averagePlayers: 0,
+                trackedPlayers: [new TrackedPlayers("23"), new TrackedPlayers("66")],
+              },
+            ],
+          },
+        ];
+
+        this.result = XXXaddCurrentPlayersFromSteam(this.playersFromSteam, this.games);
+      });
+
+      it("will have a property called 'average players', which will equal '40.3'", function () {
+        expect(this.result[0].playerHistory[0].averagePlayers).toBe("40.3");
+      });
+      it("will have a property called 'tracked players' its first array entry will be an instance of 'TrackedPlayers'", function () {
+        expect(this.result[0].playerHistory[0].trackedPlayers[0]).toBeInstanceOf(
+          TrackedPlayers,
+        );
+      });
+    });
+  });
+
+  describe(".parsePlayerHistory", function () {
+    describe("if the game has multiple histories", function () {
+      beforeEach(function () {
+        this.playerHistories = parsePlayerHistory(eldenRingHttpDetailsSteamcharts);
+      });
+
+      it("playerHistories is an instance of 'TrackedPlayers'", function () {
+        expect(this.playerHistories[0]).toBeInstanceOf(TrackedPlayers);
+      });
+
+      it("playerHistories has two entries", function () {
+        expect(this.playerHistories.length).toBe(2);
+      });
+
+      it("the first entries' player amount is 522066.4", function () {
+        expect(this.playerHistories[0].players).toBe(522066.4);
+      });
+
+      it("the first entries' month is March", function () {
+        expect(this.playerHistories[0].date.getMonth()).toBe(2);
+      });
+
+      it("the first entries' year is 2022", function () {
+        expect(this.playerHistories[0].date.getFullYear()).toBe(2022);
+      });
+    });
+
+    describe("if the game has just been released and only has one history", function () {
+      beforeEach(function () {
+        this.playerHistories = parsePlayerHistory(sniperEliteHttpDetailsSteamcharts);
+      });
+
+      it("it returns an empty array", function () {
+        expect(this.playerHistories).toEqual([]);
       });
     });
   });
