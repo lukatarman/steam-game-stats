d9b3be7dc1e24463348af937f75be9b7ca4247ad
Luka Tarman <luka.tarman@gmail.com>
2022-11-21 13:08:55 +0100

    further improved test descriptions

--- src/features/player-history-aggregator/services/player.history.service.spec.js
+++ src/features/player-history-aggregator/services/player.history.service.spec.js
@@ -36,7 +36,7 @@ describe("player.history.service.js", function () {
       });
     });
 
-    fdescribe("when the game's 'playerHistory' array includes this month's entry", function () {
+    fdescribe("when the game's 'playerHistory' array includes this month's entry, 'this.results[0].playerHistory[0]'", function () {
       beforeEach(function () {
         this.playersFromSteam = ["32"];
 
@@ -54,20 +54,20 @@ describe("player.history.service.js", function () {
         this.result = addCurrentPlayersFromSteam(this.playersFromSteam, this.games);
       });
 
-      it("'this.results[0].playerHistory[0]' is an instance of Players", function () {
+      it("is an instance of Players", function () {
         expect(this.result[0].playerHistory[0]).toBeInstanceOf(Players);
       });
-      it("'this.result[0].playerHistory[0]' has a property called 'average players', which equals '40.3'", function () {
+      it("has a property called 'average players', which equals '40.3'", function () {
         expect(this.result[0].playerHistory[0].averagePlayers).toBe(40.3);
       });
-      it("'this.result[0].playerHistory[0].trackedPlayers[0]' is an instance of 'TrackedPlayers'", function () {
+      it("is an instance of 'TrackedPlayers'", function () {
         expect(this.result[0].playerHistory[0].trackedPlayers[0]).toBeInstanceOf(
           TrackedPlayers,
         );
       });
     });
 
-    describe("when the game's 'playerHistory' array is not empty, and it does not include this month's entry", function () {
+    describe("when the game's 'playerHistory' array is not empty, and it does not include this month's entry, 'this.results[0].", function () {
       beforeEach(function () {
         this.playersFromSteam = ["32"];
         this.date = new Date("2021");
@@ -90,22 +90,22 @@ describe("player.history.service.js", function () {
         this.result = addCurrentPlayersFromSteam(this.playersFromSteam, this.games);
       });
 
-      it("'this.results[0].playerHistory[0]' has a property called 'average players', which equals '0'", function () {
+      it("playerHistory[0]' has a property called 'average players', which equals '0'", function () {
         expect(this.result[0].playerHistory[0].averagePlayers).toBe(0);
       });
-      it("'this.results[0].playerHistory[0]' has a property called 'tracked players' its first array entry is an instance of 'TrackedPlayers'", function () {
+      it("playerHistory[0]' has a property called 'tracked players' its first array entry is an instance of 'TrackedPlayers'", function () {
         expect(this.result[0].playerHistory[0].trackedPlayers[0]).toBeInstanceOf(
           TrackedPlayers,
         );
       });
-      it("'this.results[0].playerHistory[1]' is an instance of Players", function () {
+      it("playerHistory[1]' is an instance of Players", function () {
         expect(this.result[0].playerHistory[1]).toBeInstanceOf(Players);
       });
     });
   });
 
   describe(".addPlayerHistoriesFromSteamcharts uses the gamesPagesMap to add the player histories to each entry in the games array, and then returns it. So,", function () {
-    describe("if the gamesPagesMap includes Elden Ring's player history", function () {
+    describe("if the gamesPagesMap includes Elden Ring's player history, 'this.result[0].", function () {
       beforeEach(function () {
         this.map = new Map();
         this.page = eldenRingHttpDetailsSteamcharts;
@@ -121,22 +121,22 @@ describe("player.history.service.js", function () {
         this.result = addPlayerHistoriesFromSteamcharts(this.map);
       });
 
-      it("'this.result[0].playerHistory[0]' is an instance of 'Players'", function () {
+      it("playerHistory[0]' is an instance of 'Players'", function () {
         expect(this.result[0].playerHistory[0]).toBeInstanceOf(Players);
       });
-      it("'this.result[0].playerHistory[0]' has a property called 'year', which equals '2022'", function () {
+      it("playerHistory[0]' has a property called 'year', which equals '2022'", function () {
         expect(this.result[0].playerHistory[0].year).toBe(2022);
       });
-      it("'this.result[0].playerHistory[0]' has a property called 'month', which equals '2'", function () {
+      it("playerHistory[0]' has a property called 'month', which equals '2'", function () {
         expect(this.result[0].playerHistory[0].month).toBe(2);
       });
-      it("'this.result[0].playerHistory[0]' has a property called 'averagePlayers', which equals '522066.4'", function () {
+      it("playerHistory[0]' has a property called 'averagePlayers', which equals '522066.4'", function () {
         expect(this.result[0].playerHistory[0].averagePlayers).toBe(522066.4);
       });
-      it("'this.result[0].playerHistory[1]' has a property called month, which equals '3'", function () {
+      it("playerHistory[1]' has a property called month, which equals '3'", function () {
         expect(this.result[0].playerHistory[1].month).toBe(3);
       });
-      it("'this.result[0].playerHistory[1]' has a property called averagePlayers, which equals '211468.9'", function () {
+      it("playerHistory[1]' has a property called averagePlayers, which equals '211468.9'", function () {
         expect(this.result[0].playerHistory[1].averagePlayers).toBe(211468.9);
       });
     });
