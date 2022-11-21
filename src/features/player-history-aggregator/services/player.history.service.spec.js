0f29c2ec165635dc401ec2f32966db51a6a32dba
Luka Tarman <luka.tarman@gmail.com>
2022-11-21 13:06:12 +0100

    improved test descriptions

--- src/features/player-history-aggregator/services/player.history.service.spec.js
+++ src/features/player-history-aggregator/services/player.history.service.spec.js
@@ -8,7 +8,7 @@ import { eldenRingHttpDetailsSteamcharts } from "../../../../assets/steamcharts-
 
 describe("player.history.service.js", function () {
   describe(".addCurrentPlayersFromSteam adds the current players to the games object and calculates the average players. So,", function () {
-    describe("when the game's 'playerHistory' array is empty, the first array entry in this.result[0].playerHistory'", function () {
+    describe("when the game's 'playerHistory' array is empty, this.result[0].playerHistory[0]'", function () {
       beforeEach(function () {
         this.playersFromSteam = ["32"];
 
@@ -36,7 +36,7 @@ describe("player.history.service.js", function () {
       });
     });
 
-    describe("if the game's 'playerHistory' array includes this month's entry", function () {
+    fdescribe("when the game's 'playerHistory' array includes this month's entry", function () {
       beforeEach(function () {
         this.playersFromSteam = ["32"];
 
@@ -44,31 +44,30 @@ describe("player.history.service.js", function () {
           {
             id: 1,
             name: "Random Game",
-            playerHistory: [
-              {
-                year: new Date().getFullYear(),
-                month: new Date().getMonth(),
-                averagePlayers: 0,
-                trackedPlayers: [new TrackedPlayers("23"), new TrackedPlayers("66")],
-              },
-            ],
+            playerHistory: [Players.newMonthlyEntry()],
           },
         ];
+
+        this.games[0].playerHistory[0].trackedPlayers.push(new TrackedPlayers("23"));
+        this.games[0].playerHistory[0].trackedPlayers.push(new TrackedPlayers("66"));
 
         this.result = addCurrentPlayersFromSteam(this.playersFromSteam, this.games);
       });
 
-      it("will have a property called 'average players', which will equal '40.3'", function () {
+      it("'this.results[0].playerHistory[0]' is an instance of Players", function () {
+        expect(this.result[0].playerHistory[0]).toBeInstanceOf(Players);
+      });
+      it("'this.result[0].playerHistory[0]' has a property called 'average players', which equals '40.3'", function () {
         expect(this.result[0].playerHistory[0].averagePlayers).toBe(40.3);
       });
-      it("will have a property called 'tracked players'. Its first array entry will be an instance of 'TrackedPlayers'", function () {
+      it("'this.result[0].playerHistory[0].trackedPlayers[0]' is an instance of 'TrackedPlayers'", function () {
         expect(this.result[0].playerHistory[0].trackedPlayers[0]).toBeInstanceOf(
           TrackedPlayers,
         );
       });
     });
 
-    describe("if the game's 'playerHistory' array is not empty, and it does not include this month's entry", function () {
+    describe("when the game's 'playerHistory' array is not empty, and it does not include this month's entry", function () {
       beforeEach(function () {
         this.playersFromSteam = ["32"];
         this.date = new Date("2021");
@@ -91,15 +90,15 @@ describe("player.history.service.js", function () {
         this.result = addCurrentPlayersFromSteam(this.playersFromSteam, this.games);
       });
 
-      it("will have a property called 'average players', which will equal '0'", function () {
+      it("'this.results[0].playerHistory[0]' has a property called 'average players', which equals '0'", function () {
         expect(this.result[0].playerHistory[0].averagePlayers).toBe(0);
       });
-      it("will have a property called 'tracked players' its first array entry will be an instance of 'TrackedPlayers'", function () {
+      it("'this.results[0].playerHistory[0]' has a property called 'tracked players' its first array entry is an instance of 'TrackedPlayers'", function () {
         expect(this.result[0].playerHistory[0].trackedPlayers[0]).toBeInstanceOf(
           TrackedPlayers,
         );
       });
-      it("the playerHistory array will have an entry that will be an instance of Players", function () {
+      it("'this.results[0].playerHistory[1]' is an instance of Players", function () {
         expect(this.result[0].playerHistory[1]).toBeInstanceOf(Players);
       });
     });
@@ -122,22 +121,22 @@ describe("player.history.service.js", function () {
         this.result = addPlayerHistoriesFromSteamcharts(this.map);
       });
 
-      it("the first value of the property 'playerHistory' of the resulting array's first entry is an instance of 'Players'", function () {
+      it("'this.result[0].playerHistory[0]' is an instance of 'Players'", function () {
         expect(this.result[0].playerHistory[0]).toBeInstanceOf(Players);
       });
-      it("the 'year' value in the first entry of the playerHistory array has the value of '2022'", function () {
+      it("'this.result[0].playerHistory[0]' has a property called 'year', which equals '2022'", function () {
         expect(this.result[0].playerHistory[0].year).toBe(2022);
       });
-      it("the 'month' value in the first entry of the playerHistory array has the value of '2'", function () {
+      it("'this.result[0].playerHistory[0]' has a property called 'month', which equals '2'", function () {
         expect(this.result[0].playerHistory[0].month).toBe(2);
       });
-      it("the 'averagePlayers' value in the first entry of the playerHistory array has the value of '522066.4'", function () {
+      it("'this.result[0].playerHistory[0]' has a property called 'averagePlayers', which equals '522066.4'", function () {
         expect(this.result[0].playerHistory[0].averagePlayers).toBe(522066.4);
       });
-      it("the 'month' value in the second entry of the playerHistory array has the value of '3'", function () {
+      it("'this.result[0].playerHistory[1]' has a property called month, which equals '3'", function () {
         expect(this.result[0].playerHistory[1].month).toBe(3);
       });
-      it("the 'averagePlayers' value in the second entry of the playerHistory array has the value of '211468.9'", function () {
+      it("'this.result[0].playerHistory[1]' has a property called averagePlayers, which equals '211468.9'", function () {
         expect(this.result[0].playerHistory[1].averagePlayers).toBe(211468.9);
       });
     });
