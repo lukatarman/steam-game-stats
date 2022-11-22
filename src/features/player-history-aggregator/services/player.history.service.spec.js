1b2e39488dd46dc68447ae14f87f9b582f60303a
Luka Tarman <luka.tarman@gmail.com>
2022-11-22 09:56:07 +0100

    added tests to reflect new code

--- src/features/player-history-aggregator/services/player.history.service.spec.js
+++ src/features/player-history-aggregator/services/player.history.service.spec.js
@@ -140,5 +140,52 @@ describe("player.history.service.js", function () {
         expect(this.result[0].playerHistory[1].averagePlayers).toBe(211468.9);
       });
     });
+
+    describe("if the gamesPagesMap includes Elden Ring's player history, and the playerHistory already contains even more recent histories 'this.result[0].", function () {
+      beforeEach(function () {
+        this.map = new Map();
+        this.page = eldenRingHttpDetailsSteamcharts;
+
+        this.game = {
+          id: 1,
+          name: "Elden Ring",
+          playerHistory: [
+            {
+              year: 2022,
+              month: 11,
+            },
+          ],
+        };
+
+        this.map.set(this.game, this.page);
+
+        this.result = addPlayerHistoriesFromSteamcharts(this.map);
+      });
+
+      it("playerHistory[0]' is an instance of 'Players'", function () {
+        expect(this.result[0].playerHistory[0]).toBeInstanceOf(Players);
+      });
+      it("playerHistory[0]' has a property called 'year', which equals '2022'", function () {
+        expect(this.result[0].playerHistory[0].year).toBe(2022);
+      });
+      it("playerHistory[0]' has a property called 'month', which equals '2'", function () {
+        expect(this.result[0].playerHistory[0].month).toBe(2);
+      });
+      it("playerHistory[0]' has a property called 'averagePlayers', which equals '522066.4'", function () {
+        expect(this.result[0].playerHistory[0].averagePlayers).toBe(522066.4);
+      });
+      it("playerHistory[1]' has a property called month, which equals '3'", function () {
+        expect(this.result[0].playerHistory[1].month).toBe(3);
+      });
+      it("playerHistory[1]' has a property called averagePlayers, which equals '211468.9'", function () {
+        expect(this.result[0].playerHistory[1].averagePlayers).toBe(211468.9);
+      });
+      it("playerHistory[2]' has a property called year, which equals '2022'", function () {
+        expect(this.result[0].playerHistory[2].year).toBe(2022);
+      });
+      it("playerHistory[2]' has a property called month, which equals '11'", function () {
+        expect(this.result[0].playerHistory[2].month).toBe(11);
+      });
+    });
   });
 });
