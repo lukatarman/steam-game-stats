dc415eee00f163febed6eac22dbd109d64e645e4
Luka Tarman <luka.tarman@gmail.com>
2022-12-14 11:01:56 +0100

    added tests for manyFromDbEntry

--- src/models/players.spec.js
+++ src/models/players.spec.js
@@ -41,6 +41,64 @@ describe("Players", function () {
     });
   });
 
+  describe(".manyFromDbEntry creates a list of Player instances from a list of history objects. So,", function () {
+    describe("when a list of history objects is passed in", function () {
+      beforeEach(function () {
+        this.histories = [
+          {
+            year: "2022",
+            month: "11",
+            averagePlayers: 34,
+            trackedPlayers: [],
+          },
+          {
+            year: "2022",
+            month: "10",
+            averagePlayers: 78,
+            trackedPlayers: [],
+          },
+        ];
+
+        this.results = Players.manyFromDbEntry(this.histories);
+      });
+
+      it("the first result is an instance of Players.", function () {
+        expect(this.results[0]).toBeInstanceOf(Players);
+      });
+      it("the first result has a property called year, which equals 2022.", function () {
+        expect(this.results[0].year).toBe("2022");
+      });
+      it("the first result has a property called month, which equals 11.", function () {
+        expect(this.results[0].month).toBe("11");
+      });
+      it("the first result has a property called averagePlayers, which equals 34", function () {
+        expect(this.results[0].averagePlayers).toBe(34);
+      });
+      it("the second result is an instance of Players.", function () {
+        expect(this.results[1]).toBeInstanceOf(Players);
+      });
+      it("the second result has a property called year, which equals 2022.", function () {
+        expect(this.results[1].year).toBe("2022");
+      });
+      it("the second result has a property called month, which equals 10.", function () {
+        expect(this.results[1].month).toBe("10");
+      });
+      it("the second result has a property called averagePlayers, which equals 78", function () {
+        expect(this.results[1].averagePlayers).toBe(78);
+      });
+    });
+
+    describe("when the passed in array is empty", function () {
+      beforeEach(function () {
+        this.results = Players.manyFromSteamchartsPage([]);
+      });
+
+      it("the returned array's will be empty", function () {
+        expect(this.results).toEqual([]);
+      });
+    });
+  });
+
   describe(".newMonthlyEntry instantiates the Players class with default values. So, the returned object", function () {
     beforeEach(function () {
       this.currentDate = new Date();
