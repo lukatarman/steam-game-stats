7e74f9af8efe700fc7e9e0ad93df28749efa2932
Luka Tarman <luka.tarman@gmail.com>
2022-12-15 10:18:48 +0100

    improved test descriptions

--- src/models/game.spec.js
+++ src/models/game.spec.js
@@ -113,9 +113,9 @@ describe("game.js", function () {
       });
     });
 
-    describe(".addOnePlayerHistoryEntry adds a trackedPlayers entry to the correct playerHistory object and returns the object. So,", function () {
-      describe("when this month's entry already exists", function () {
-        describe("players get added into the existing playerHistory entry. So,", function () {
+    describe(".addOnePlayerHistoryEntry adds a trackedPlayers entry to the correct playerHistory array entry.", function () {
+      describe("When this month's entry already exists,", function () {
+        describe("players get added into the existing playerHistory entry.", function () {
           beforeEach(function () {
             this.currentPlayers = 45;
             this.playerHistory = [
@@ -139,17 +139,17 @@ describe("game.js", function () {
             this.result.addOnePlayerHistoryEntry(this.currentPlayers);
           });
 
-          it("the resulting object's playerHistory value is an instance of Players.", function () {
+          it("The resulting object's playerHistory value is an instance of Players.", function () {
             expect(this.result.playerHistory[0]).toBeInstanceOf(Players);
           });
-          it("the resulting object has a property called players, which equals 45", function () {
+          it("The resulting object has a property called players, which equals 45", function () {
             expect(this.result.playerHistory[0].trackedPlayers[0].players).toBe(45);
           });
         });
       });
 
-      describe("when this month's entry does not exist yet", function () {
-        describe("players get added into a new playerHistory entry. So,", function () {
+      describe("When this month's entry does not exist yet", function () {
+        describe("players get added into a new playerHistory entry.", function () {
           beforeEach(function () {
             this.currentPlayers = 33;
             this.playerHistory = [
@@ -173,10 +173,10 @@ describe("game.js", function () {
             this.result.addOnePlayerHistoryEntry(this.currentPlayers);
           });
 
-          it("the resulting object's playerHistory value has a length of 1", function () {
+          it("The resulting object's playerHistory value has a length of 1", function () {
             expect(this.result.playerHistory.length).toBe(2);
           });
-          it("the resulting object's second playerHistory entry has a property called players, which equals 45", function () {
+          it("The resulting object's second playerHistory entry has a property called players, which equals 45", function () {
             expect(this.result.playerHistory[1].trackedPlayers[0].players).toBe(33);
           });
         });
@@ -184,7 +184,7 @@ describe("game.js", function () {
     });
 
     describe(".addHistoryEntriesFromSteamcharts", function () {
-      describe("adds a game's Steamcharts history entries in the correct format. So, ", function () {
+      describe("adds a game's Steamcharts history entries in the correct format. ", function () {
         beforeEach(function () {
           this.steamApp = {
             appid: 1,
@@ -211,13 +211,13 @@ describe("game.js", function () {
           this.result.addHistoryEntriesFromSteamcharts(this.gameHistories);
         });
 
-        it("the resulting object's playerHistory value is an instance of Players", function () {
+        it("The resulting object's playerHistory value is an instance of Players", function () {
           expect(this.result.playerHistory[0]).toBeInstanceOf(Players);
         });
-        it("the resulting object's playerHistory array has a length of 3", function () {
+        it("The resulting object's playerHistory array has a length of 3", function () {
           expect(this.result.playerHistory.length).toBe(3);
         });
-        it("the resulting object's playerHistory array is in the correct order", function () {
+        it("The resulting object's playerHistory array is in the correct order", function () {
           expect(this.result.playerHistory[0].averagePlayers).toBe(55);
           expect(this.result.playerHistory[1].averagePlayers).toBe(5);
           expect(this.result.playerHistory[2].averagePlayers).toBe(15);
