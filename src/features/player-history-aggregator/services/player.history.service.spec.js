1166d9bbbb5f42712a44e0cdd13baaadcc03acb9
Luka Tarman <luka.tarman@gmail.com>
2022-12-20 10:15:13 +0100

    removed deprecated test

--- src/features/player-history-aggregator/services/player.history.service.spec.js
+++ src/features/player-history-aggregator/services/player.history.service.spec.js
@@ -7,59 +7,6 @@ import { eldenRingHttpDetailsSteamcharts } from "../../../../assets/steamcharts-
 import { crushTheCastleHtmlDetailsSteamcharts } from "../../../../assets/steamcharts-details-pages/crush.the.castle.legacy.collection.html.details.page.js";
 
 describe("player.history.service.js", function () {
-  describe(".addCurrentPlayersFromSteam runs a function on each value of the provided games array.", function () {
-    describe("When the players array has player values", function () {
-      beforeEach(function () {
-        this.firstGame = { id: 1, name: "Game 1", playerHistory: [] };
-        this.SecondGame = { id: 2, name: "Game 2", playerHistory: [] };
-
-        this.players = [55, 45];
-
-        this.games = [
-          Game.fromDbEntry(this.firstGame),
-          Game.fromDbEntry(this.SecondGame),
-        ];
-
-        this.result = addCurrentPlayersFromSteam(this.players, this.games);
-      });
-
-      it("the resulting array has a length of 2", function () {
-        expect(this.result.length).toBe(2);
-      });
-      it("the first result has a value of average players, which is 55", function () {
-        expect(this.result[0].playerHistory[0].averagePlayers).toBe(55);
-      });
-      it("the second result has a value of average players, which is 45", function () {
-        expect(this.result[1].playerHistory[0].averagePlayers).toBe(45);
-      });
-    });
-
-    describe("When the players array has zeroes", function () {
-      beforeEach(function () {
-        this.firstGame = { id: 1, name: "Game 1", playerHistory: [] };
-        this.SecondGame = { id: 2, name: "Game 2", playerHistory: [] };
-
-        this.games = [
-          Game.fromDbEntry(this.firstGame),
-          Game.fromDbEntry(this.SecondGame),
-        ];
-
-        this.players = [0, 0];
-
-        this.result = addCurrentPlayersFromSteam(this.players, this.games);
-      });
-      it("the result is an empty array", function () {
-        expect(this.result.length).toBe(2);
-      });
-      it("the first result has a property 'averagePlayers', which equals 0", function () {
-        expect(this.result[0].playerHistory[0].averagePlayers).toBe(0);
-      });
-      it("the second result has a property 'averagePlayers', which equals 0", function () {
-        expect(this.result[1].playerHistory[0].averagePlayers).toBe(0);
-      });
-    });
-  });
-
   describe(".addPlayerHistoriesFromSteamcharts adds the player histores from Steamcharts to each game object", function () {
     describe("When both gamesPagesMap properties have proper values", function () {
       beforeEach(function () {
