994682e9c833c2b323a5bae8f63c536960485b33
Luka Tarman <luka.tarman@gmail.com>
2023-05-12 15:32:19 +0200

    added additional functions to game service
    # added getReleaseDate function
    # added getDevelopers function
    # added tests

--- src/features/game-identifier/services/game.service.js
+++ src/features/game-identifier/services/game.service.js
@@ -24,11 +24,33 @@ export function getSteamAppType(httpDetailsPage) {
 export function discoverGamesFromSteamWeb(steamApps, htmlDetailsPages) {
   return htmlDetailsPages
     .map((page, i) => {
+      const releaseDate = getReleaseDate(htmlDetailsPages);
+      const developers = getDevelopers(htmlDetailsPages);
       if (getSteamAppType(page) === SteamApp.validTypes.game) {
-        return Game.fromSteamApp(steamApps[i], new JSDOM(page));
+        return Game.fromSteamApp(steamApps[i], releaseDate, developers);
       }
     })
     .filter((game) => !!game);
+}
+
+export function getReleaseDate(page) {
+  const dom = new JSDOM(page);
+
+  const releaseDate = dom.window.document.querySelector(".release_date .date");
+
+  if (!releaseDate) return "";
+
+  return releaseDate.textContent;
+}
+
+export function getDevelopers(page) {
+  const dom = new JSDOM(page);
+
+  const developers = dom.window.document.querySelector(".dev_row #developers_list");
+
+  if (!developers) return [];
+
+  return Array.from(developers.children).map((developer) => developer.textContent);
 }
 
 export function updateTypeSideEffectFree(steamApps, htmlDetailsPages) {
