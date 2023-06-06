0916152387e3b8abccc10bff2875c50fa66c7ed8
Luka Tarman <luka.tarman@gmail.com>
2023-06-06 12:27:26 +0200

    added argument for collection initialization

--- src/infrastructure/database/in.memory.database.client.js
+++ src/infrastructure/database/in.memory.database.client.js
@@ -1,13 +1,13 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import { DatabaseClient } from "./database.client.js";
 
-export const initiateInMemoryDatabase = async () => {
+export const initiateInMemoryDatabase = async (collections) => {
   const mongoServer = await MongoMemoryServer.create();
 
   const databaseOptions = {
     url: mongoServer.getUri(),
     databaseName: "game-stats",
-    collections: ["games", "history_checks"],
+    collections: collections,
   };
 
   const databaseClient = await new DatabaseClient().init(databaseOptions);
