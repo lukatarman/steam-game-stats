39f3acd916c0c47dac74661acac4fda4341699c4
Luka Tarman <luka.tarman@gmail.com>
2023-06-06 12:27:35 +0200

    added test for insertManyGames method

--- src/infrastructure/database/repositories/games.repository.spec.js
+++ src/infrastructure/database/repositories/games.repository.spec.js
@@ -2,6 +2,60 @@ import { GamesRepository } from "./games.repository.js";
 import { initiateInMemoryDatabase } from "../in.memory.database.client.js";
 
 describe("games.repository.js", function () {
+  describe(".insertManyGames inserts an array ob objects into the database.", function () {
+    describe("If two games are inserted,", function () {
+      beforeAll(async function () {
+        this.databaseClient = await initiateInMemoryDatabase(["games", "history_checks"]);
+
+        const gamesRepo = new GamesRepository(this.databaseClient);
+
+        await gamesRepo.insertManyGames([
+          { id: 1, name: "First Game" },
+          { id: 2, name: "Second Game" },
+        ]);
+
+        this.result = await this.databaseClient
+          .get("games")
+          .find({ id: { $ne: 0 } })
+          .toArray();
+      });
+
+      afterAll(function () {
+        this.databaseClient.disconnect();
+      });
+
+      it("the resulting array has a length of 2", function () {
+        expect(this.result.length).toBe(2);
+      });
+
+      it("the first array's name property is 'First Game' and its id is '1'", function () {
+        expect(this.result[0].id).toBe(1);
+        expect(this.result[0].name).toBe("First Game");
+      });
+
+      it("the second array's name property is 'Second Game' and its id is '2'", function () {
+        expect(this.result[1].id).toBe(2);
+        expect(this.result[1].name).toBe("Second Game");
+      });
+    });
+
+    fdescribe("If nothing is inserted, the result", function () {
+      beforeAll(async function () {
+        this.databaseClient = await initiateInMemoryDatabase(["games", "history_checks"]);
+
+        this.result = await this.databaseClient.get("games").find({}).toArray();
+      });
+
+      afterAll(function () {
+        this.databaseClient.disconnect();
+      });
+
+      it("the result is an empty array", function () {
+        expect(this.result).toEqual([]);
+      });
+    });
+  });
+
   describe(".getOneGameById returns a document based on the id passed in.", function () {
     describe("If an existing id is passed in, the resulting object ", function () {
       beforeAll(async function () {
