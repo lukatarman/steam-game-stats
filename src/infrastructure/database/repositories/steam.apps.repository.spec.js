38afee827f365180f6b7ce877dae991e4c131683
Luka Tarman <luka.tarman@gmail.com>
2023-06-09 12:41:00 +0200

    fixed/improved test

--- src/infrastructure/database/repositories/steam.apps.repository.spec.js
+++ src/infrastructure/database/repositories/steam.apps.repository.spec.js
@@ -2,7 +2,7 @@ import { SteamApp } from "../../../models/steam.app.js";
 import { initiateInMemoryDatabase } from "../in.memory.database.client.js";
 import { SteamAppsRepository } from "./steam.apps.repository.js";
 
-describe("steam.apps.repository.js", function () {
+describe("SteamAppsRepository", function () {
   describe(".insertManySteamApps adds provided apps into the database", function () {
     describe("If two apps are provided,", function () {
       beforeAll(async function () {
@@ -16,6 +16,7 @@ describe("steam.apps.repository.js", function () {
         ];
 
         await steamAppsRepo.insertManySteamApps(apps);
+
         this.result = await this.databaseClient.getAll("steam_apps");
       });
 
@@ -37,25 +38,9 @@ describe("steam.apps.repository.js", function () {
         expect(this.result[1].name).toBe("Risk of Stain");
       });
     });
-
-    describe("If no apps are provided,", function () {
-      beforeAll(async function () {
-        this.databaseClient = await initiateInMemoryDatabase(["steam_apps"]);
-
-        this.result = await this.databaseClient.getAll("steam_apps");
-      });
-
-      afterAll(function () {
-        this.databaseClient.disconnect();
-      });
-
-      it("the result is an empty array", function () {
-        expect(this.result).toEqual([]);
-      });
-    });
   });
 
-  describe(".getAllSteamApps returns the results from the database as instances of SteamApp", function () {
+  describe(".getAllSteamApps all documents from the database as instances of SteamApp", function () {
     describe("If two steam apps exist in the collection", function () {
       beforeAll(async function () {
         this.databaseClient = await initiateInMemoryDatabase(["steam_apps"]);
@@ -105,26 +90,10 @@ describe("steam.apps.repository.js", function () {
         expect(this.result[1].type).toBe("downloadableContent");
       });
     });
-
-    describe("If no steam apps exist in the collection,", function () {
-      beforeAll(async function () {
-        this.databaseClient = await initiateInMemoryDatabase(["steam_apps"]);
-
-        this.result = await this.databaseClient.getAll("steam_apps");
-      });
-
-      afterAll(function () {
-        this.databaseClient.disconnect();
-      });
-
-      it("the result is an empty array", function () {
-        expect(this.result).toEqual([]);
-      });
-    });
   });
 
-  describe(".updateSteamAppsById updates the steam apps collections with new values", function () {
-    describe("If two steam apps are provided for an update", function () {
+  describe(".updateSteamAppsById updates the values of the matching provided steam apps", function () {
+    describe("If two steam provided steam apps match", function () {
       beforeAll(async function () {
         this.databaseClient = await initiateInMemoryDatabase(["steam_apps"]);
 
@@ -170,7 +139,7 @@ describe("steam.apps.repository.js", function () {
         this.databaseClient.disconnect();
       });
 
-      it("the resulting array has a length of 2", function () {
+      it("the resulting array has a length of 3", function () {
         expect(this.result.length).toBe(3);
       });
 
@@ -187,23 +156,12 @@ describe("steam.apps.repository.js", function () {
         expect(this.result[1].type).toBe("downloadableContent");
         expect(this.result[1].triedVia).toBe("steamWeb");
       });
-    });
 
-    describe("If no apps are provided,", function () {
-      beforeAll(async function () {
-        this.databaseClient = await initiateInMemoryDatabase(["steam_apps"]);
-
-        const steamAppsRepo = new SteamAppsRepository(this.databaseClient);
-
-        this.result = await steamAppsRepo.updateSteamAppsById([]);
-      });
-
-      afterAll(function () {
-        this.databaseClient.disconnect();
-      });
-
-      it("the result is an empty array", function () {
-        expect(this.result).toBe(undefined);
+      it("the third array has the correct values", function () {
+        expect(this.result[2].appid).toBe(3);
+        expect(this.result[2].name).toBe("Risk of Sprain");
+        expect(this.result[2].type).toBe("unknown");
+        expect(this.result[2].triedVia).toEqual([]);
       });
     });
   });
@@ -288,7 +246,7 @@ describe("steam.apps.repository.js", function () {
         expect(this.result[0].triedVia[0]).toBe("steamcharts");
       });
 
-      it("the first result is an instance of SteamApp", function () {
+      it("the second result is an instance of SteamApp", function () {
         expect(this.result[1]).toBeInstanceOf(SteamApp);
       });
 
@@ -300,7 +258,109 @@ describe("steam.apps.repository.js", function () {
       });
     });
 
-    describe("If '0' amount is provided,", function () {
+    describe("If three steam apps out of eigth match the filters, and the amount of 4 is provided", function () {
+      beforeAll(async function () {
+        this.databaseClient = await initiateInMemoryDatabase(["steam_apps"]);
+
+        this.databaseClient.insertMany("steam_apps", [
+          {
+            appid: 1,
+            name: "Risk of Strain",
+            type: "unknown",
+            triedVia: ["steamcharts"],
+          },
+          {
+            appid: 2,
+            name: "Risk of Stain",
+            type: "unknown",
+            triedVia: ["steamcharts"],
+          },
+          {
+            appid: 3,
+            name: "Risk of Sprain",
+            type: "game",
+            triedVia: ["steamcharts"],
+          },
+          {
+            appid: 4,
+            name: "Risk of Rain",
+            type: "unknown",
+            triedVia: ["steamcharts", "steamWeb"],
+          },
+          {
+            appid: 5,
+            name: "Risk of Train Soundtrack",
+            type: "unknown",
+            triedVia: ["steamcharts"],
+          },
+          {
+            appid: 6,
+            name: "Risk of Cane DLC",
+            type: "unknown",
+            triedVia: ["steamcharts"],
+          },
+          {
+            appid: 7,
+            name: "Risk of Plain Demo",
+            type: "unknown",
+            triedVia: ["steamcharts"],
+          },
+          {
+            appid: 8,
+            name: "Risk of Crane",
+            type: "unknown",
+            triedVia: ["steamcharts"],
+          },
+        ]);
+
+        const steamAppsRepo = new SteamAppsRepository(this.databaseClient);
+
+        this.result = await steamAppsRepo.getSteamWebUntriedFilteredSteamApps(4);
+      });
+
+      afterAll(function () {
+        this.databaseClient.disconnect();
+      });
+
+      it("the resulting array has a length of 3", function () {
+        expect(this.result.length).toBe(3);
+      });
+
+      it("the first result is an instance of SteamApp", function () {
+        expect(this.result[0]).toBeInstanceOf(SteamApp);
+      });
+
+      it("the first array has the correct values", function () {
+        expect(this.result[0].appid).toBe(1);
+        expect(this.result[0].name).toBe("Risk of Strain");
+        expect(this.result[0].type).toBe("unknown");
+        expect(this.result[0].triedVia[0]).toBe("steamcharts");
+      });
+
+      it("the second result is an instance of SteamApp", function () {
+        expect(this.result[1]).toBeInstanceOf(SteamApp);
+      });
+
+      it("the second array has the correct values", function () {
+        expect(this.result[1].appid).toBe(2);
+        expect(this.result[1].name).toBe("Risk of Stain");
+        expect(this.result[1].type).toBe("unknown");
+        expect(this.result[1].triedVia[0]).toBe("steamcharts");
+      });
+
+      it("the third result is an instance of SteamApp", function () {
+        expect(this.result[2]).toBeInstanceOf(SteamApp);
+      });
+
+      it("the third array has the correct values", function () {
+        expect(this.result[2].appid).toBe(8);
+        expect(this.result[2].name).toBe("Risk of Crane");
+        expect(this.result[2].type).toBe("unknown");
+        expect(this.result[2].triedVia[0]).toBe("steamcharts");
+      });
+    });
+
+    describe("If an amount of '0' is provided,", function () {
       beforeAll(async function () {
         this.databaseClient = await initiateInMemoryDatabase(["steam_apps"]);
 
@@ -404,7 +464,7 @@ describe("steam.apps.repository.js", function () {
   });
 
   describe(".getSteamchartsUntriedFilteredSteamApps retrieves a specific set of steamApps from the database.", function () {
-    describe("If three steam apps out of eigth match the filters, and the amount of 2 is provided", function () {
+    describe("If three steam apps out of nine match the filters, and the amount of 2 is provided", function () {
       beforeAll(async function () {
         this.databaseClient = await initiateInMemoryDatabase(["steam_apps"]);
 
@@ -423,39 +483,51 @@ describe("steam.apps.repository.js", function () {
           },
           {
             appid: 3,
+            name: "Risk of Gain",
+            type: "unknown",
+            triedVia: ["steamWeb"],
+          },
+          {
+            appid: 4,
             name: "Risk of Sprain",
             type: "game",
             triedVia: ["steamWeb"],
           },
           {
-            appid: 4,
+            appid: 5,
             name: "Risk of Rain",
             type: "unknown",
             triedVia: ["steamcharts"],
           },
           {
-            appid: 5,
+            appid: 6,
             name: "Risk of Train",
             type: "unknown",
             triedVia: [],
           },
           {
-            appid: 6,
+            appid: 7,
             name: "Risk of Cane Soundtrack",
             type: "unknown",
             triedVia: ["steamWeb"],
           },
           {
-            appid: 7,
+            appid: 8,
             name: "Risk of Plain DLC",
             type: "unknown",
             triedVia: ["steamWeb"],
           },
           {
-            appid: 8,
+            appid: 9,
             name: "Risk of Crane Demo",
             type: "unknown",
             triedVia: ["steamWeb"],
+          },
+          {
+            appid: 10,
+            name: "Risk of Lane",
+            type: "unknown",
+            triedVia: ["steamWeb, steamcharts"],
           },
         ]);
 
@@ -483,7 +555,7 @@ describe("steam.apps.repository.js", function () {
         expect(this.result[0].triedVia[0]).toBe("steamWeb");
       });
 
-      it("the first result is an instance of SteamApp", function () {
+      it("the second result is an instance of SteamApp", function () {
         expect(this.result[1]).toBeInstanceOf(SteamApp);
       });
 
@@ -495,7 +567,7 @@ describe("steam.apps.repository.js", function () {
       });
     });
 
-    describe("If '0' amount is provided,", function () {
+    describe("If three steam apps out of nine match the filters, and the amount of 4 is provided", function () {
       beforeAll(async function () {
         this.databaseClient = await initiateInMemoryDatabase(["steam_apps"]);
 
@@ -514,43 +586,157 @@ describe("steam.apps.repository.js", function () {
           },
           {
             appid: 3,
+            name: "Risk of Gain",
+            type: "unknown",
+            triedVia: ["steamWeb"],
+          },
+          {
+            appid: 4,
             name: "Risk of Sprain",
             type: "game",
             triedVia: ["steamWeb"],
           },
           {
-            appid: 4,
+            appid: 5,
             name: "Risk of Rain",
             type: "unknown",
             triedVia: ["steamcharts"],
           },
           {
-            appid: 5,
+            appid: 6,
             name: "Risk of Train",
             type: "unknown",
             triedVia: [],
           },
           {
-            appid: 6,
+            appid: 7,
             name: "Risk of Cane Soundtrack",
             type: "unknown",
             triedVia: ["steamWeb"],
           },
           {
-            appid: 7,
+            appid: 8,
             name: "Risk of Plain DLC",
             type: "unknown",
             triedVia: ["steamWeb"],
           },
           {
-            appid: 8,
+            appid: 9,
             name: "Risk of Crane Demo",
             type: "unknown",
             triedVia: ["steamWeb"],
           },
           {
-            appid: 9,
+            appid: 10,
+            name: "Risk of Lane",
+            type: "unknown",
+            triedVia: ["steamWeb, steamcharts"],
+          },
+        ]);
+
+        const steamAppsRepo = new SteamAppsRepository(this.databaseClient);
+
+        this.result = await steamAppsRepo.getSteamchartsUntriedFilteredSteamApps(4);
+      });
+
+      afterAll(function () {
+        this.databaseClient.disconnect();
+      });
+
+      it("the resulting array has a length of 3", function () {
+        expect(this.result.length).toBe(3);
+      });
+
+      it("the first result is an instance of SteamApp", function () {
+        expect(this.result[0]).toBeInstanceOf(SteamApp);
+      });
+
+      it("the first array has the correct values", function () {
+        expect(this.result[0].appid).toBe(1);
+        expect(this.result[0].name).toBe("Risk of Strain");
+        expect(this.result[0].type).toBe("unknown");
+        expect(this.result[0].triedVia[0]).toBe("steamWeb");
+      });
+
+      it("the second result is an instance of SteamApp", function () {
+        expect(this.result[1]).toBeInstanceOf(SteamApp);
+      });
+
+      it("the second array has the correct values", function () {
+        expect(this.result[1].appid).toBe(2);
+        expect(this.result[1].name).toBe("Risk of Stain");
+        expect(this.result[1].type).toBe("unknown");
+        expect(this.result[1].triedVia[0]).toBe("steamWeb");
+      });
+
+      it("the third result is an instance of SteamApp", function () {
+        expect(this.result[2]).toBeInstanceOf(SteamApp);
+      });
+
+      it("the third array has the correct values", function () {
+        expect(this.result[2].appid).toBe(3);
+        expect(this.result[2].name).toBe("Risk of Gain");
+        expect(this.result[2].type).toBe("unknown");
+        expect(this.result[2].triedVia[0]).toBe("steamWeb");
+      });
+    });
+
+    fdescribe("If '0' amount is provided,", function () {
+      beforeAll(async function () {
+        this.databaseClient = await initiateInMemoryDatabase(["steam_apps"]);
+
+        this.databaseClient.insertMany("steam_apps", [
+          {
+            appid: 1,
+            name: "Risk of Strain",
+            type: "unknown",
+            triedVia: ["steamWeb"],
+          },
+          {
+            appid: 2,
+            name: "Risk of Stain",
+            type: "unknown",
+            triedVia: ["steamWeb"],
+          },
+          {
+            appid: 3,
             name: "Risk of Gain",
+            type: "unknown",
+            triedVia: ["steamWeb"],
+          },
+          {
+            appid: 4,
+            name: "Risk of Sprain",
+            type: "game",
+            triedVia: ["steamWeb"],
+          },
+          {
+            appid: 5,
+            name: "Risk of Rain",
+            type: "unknown",
+            triedVia: ["steamcharts"],
+          },
+          {
+            appid: 6,
+            name: "Risk of Train",
+            type: "unknown",
+            triedVia: [],
+          },
+          {
+            appid: 7,
+            name: "Risk of Cane Soundtrack",
+            type: "unknown",
+            triedVia: ["steamWeb"],
+          },
+          {
+            appid: 8,
+            name: "Risk of Plain DLC",
+            type: "unknown",
+            triedVia: ["steamWeb"],
+          },
+          {
+            appid: 9,
+            name: "Risk of Crane Demo",
             type: "unknown",
             triedVia: ["steamWeb"],
           },
@@ -596,7 +782,7 @@ describe("steam.apps.repository.js", function () {
       });
 
       it("the third array has the correct values", function () {
-        expect(this.result[2].appid).toBe(9);
+        expect(this.result[2].appid).toBe(3);
         expect(this.result[2].name).toBe("Risk of Gain");
         expect(this.result[2].type).toBe("unknown");
         expect(this.result[2].triedVia[0]).toBe("steamWeb");
