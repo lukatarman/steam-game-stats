26bb357cfe56565b29a987ddff07b3140adc8626
Stanislav Jakuschevskij <stanislav.jakuschevskij@at.ibm.com>
2023-08-04 17:22:22 +0200

    added logging to steam apps aggregator

--- backend/src/main.js
+++ backend/src/main.js
@@ -67,6 +67,7 @@ async function main() {
     steamClient,
     steamAppsUpdateTimestampRepository,
     steamAppsRepository,
+    logger,
     options,
   );
   const gameIdentifier = new GameIdentifier(
