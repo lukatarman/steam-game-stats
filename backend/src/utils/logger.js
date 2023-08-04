3fbdac2c5d9a4961df26a462108c000e22935fee
Stanislav Jakuschevskij <stanislav.jakuschevskij@at.ibm.com>
2023-08-04 17:21:35 +0200

    added caller to log message

--- backend/src/utils/logger.js
+++ backend/src/utils/logger.js
@@ -10,4 +10,29 @@ export const loggerConfig = {
   },
 };
 
-export const logger = pino(loggerConfig);
+const pinoLogger = pino(loggerConfig);
+
+class Logger {
+  info(msg) {
+    return pinoLogger.info(`${this.#caller} ${msg}`);
+  }
+
+  error(msg) {
+    return pinoLogger.error(`${this.#caller} ${msg}`);
+  }
+
+  warn(msg) {
+    return pinoLogger.warn(`${this.#caller} ${msg}`);
+  }
+
+  get #caller() {
+    const e = new Error();
+    const frame = e.stack.split("\n")[3];
+    const fileName = frame.split(":").reverse()[2].split("/").reverse()[0];
+    const functionName = frame.split(" ")[5];
+    const lineNumber = frame.split(":").reverse()[1];
+    return `\x1b[30m[${fileName}:${functionName}:${lineNumber}]\x1b[36m`;
+  }
+}
+
+export const logger = new Logger();
