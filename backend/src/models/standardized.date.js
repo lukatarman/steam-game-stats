acb6182e9f97e48366380add9ad688cf5002958f
Luka Tarman <luka.tarman@gmail.com>
2023-10-20 09:49:38 +0200

    Added StandardizedDate class

---
+++ backend/src/models/standardized.date.js
@@ -0,0 +1,30 @@
+export class StandardizedDate {
+  date;
+
+  constructor(date) {
+    this.date = date == "Invalid Date" ? "" : new Date(date);
+  }
+
+  static getUTCDate(rawDate) {
+    const standardizedDate = new StandardizedDate(rawDate);
+
+    if (standardizedDate.date === "") return standardizedDate;
+
+    standardizedDate.#toUTC();
+
+    return standardizedDate;
+  }
+
+  #toUTC() {
+    this.date = new Date(
+      Date.UTC(
+        this.date.getFullYear(),
+        this.date.getMonth(),
+        this.date.getDate(),
+        this.date.getHours(),
+        this.date.getMinutes(),
+        this.date.getSeconds(),
+      ),
+    ).toISOString();
+  }
+}
