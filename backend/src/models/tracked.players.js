17680ccb77279823b0e2eae6ae7cd0e9f7703b59
Luka Tarman <luka.tarman@gmail.com>
2023-10-20 10:59:29 +0200

    Used new StandardizedDate class when needed
    # removed unnecessary comment
    # adjusted code

--- backend/src/models/tracked.players.js
+++ backend/src/models/tracked.players.js
@@ -1,13 +1,10 @@
+import { StandardizedDate } from "./standardized.date.js";
+
 export class TrackedPlayers {
   date;
   players;
 
   constructor(playersAsString, dateAsString = "") {
-    // We add twelve hours to the date saved in the players class to remove any issues related to any
-    // automatic changing of dates at any point. Currently, this is relevant because mongoDB automatically converts
-    // all dates into the UTC timezone. This marks our dates as being part of the wrong month when saving
-    // the entries into the database.
-
     this.date = dateAsString === "" ? new Date() : this.#fixDate(dateAsString);
 
     this.players = parseFloat(parseFloat(playersAsString).toFixed(1));
@@ -16,17 +13,11 @@ export class TrackedPlayers {
   #fixDate(dateAsString) {
     const date = new Date(dateAsString);
 
-    if (date.getHours() === 0 && date.getMinutes() === 0 && date.getSeconds() === 0)
-      return this.#addTwelveHours(dateAsString);
+    if (date.getHours() === 0 && date.getMinutes() === 0 && date.getSeconds() === 0) {
+      return StandardizedDate.getUTCDate(dateAsString).date;
+    }
 
     return date;
-  }
-
-  #addTwelveHours(date) {
-    const dateInMs = Date.parse(date);
-    const twelveHoursInMs = 12 * 60 * 60 * 1000;
-
-    return new Date(dateInMs + twelveHoursInMs);
   }
 
   static manyFromDbEntry(trackedPlayersArray) {
