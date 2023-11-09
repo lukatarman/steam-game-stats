500d453a6cf515a7cd1c9e89139a00d851a4a322
Stanislav Jakuschevskij <stanislav.jakuschevskij@at.ibm.com>
2023-11-09 16:45:24 +0100

    removed test code

--- backend/src/main.js
+++ backend/src/main.js
@@ -19,7 +19,6 @@ import { Logger } from "./utils/logger.js";
 // our entry point = main
 async function main() {
   const logger = new Logger(config.logger.level);
-  logger.info("logger initialized");
   const databaseClient = await new DatabaseClient(logger).init(config.db);
 
   // repositories
