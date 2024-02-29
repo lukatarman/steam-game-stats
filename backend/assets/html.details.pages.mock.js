07423a8f222b149932052ea700617031aaf691f6
Luka Tarman <luka.tarman@gmail.com>
2024-02-29 23:05:49 +0100

    added functions to html details mocks

--- backend/assets/html.details.pages.mock.js
+++ backend/assets/html.details.pages.mock.js
@@ -1,5 +1,15 @@
+import { parseHTML } from "linkedom";
+
 export function createHtmlDetailsPages(pages) {
   return pages.map((page, index) => {
     return { page, id: index + 1 };
   });
+}
+
+export function getParsedHtmlPage(page) {
+  return parseHTML(page).document;
+}
+
+export function getParsedHtmlPages(pages) {
+  return pages.map((page) => parseHTML(page).document);
 }
