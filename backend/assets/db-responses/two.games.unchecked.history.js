ea0826246c632cfcfb2f6b7541a24b6dfdd349ca
Luka Tarman <luka.tarman@gmail.com>
2024-05-17 12:37:42 +0200

    ran prettier on mock file

--- backend/assets/db-responses/two.games.unchecked.history.js
+++ backend/assets/db-responses/two.games.unchecked.history.js
@@ -1,22 +1,22 @@
 export const twoGamesWithUncheckedPlayerHistory = [
   {
-    "_id": {
-      "$oid": "629b7eb2960971fa8c05ca74"
+    _id: {
+      $oid: "629b7eb2960971fa8c05ca74",
     },
-    "id": 1838970,
-    "name": "Crush the Castle Legacy Collection",
-    "image": null,
-    "imageUrl": "https://cdn.akamai.steamstatic.com/steam/apps/1838970/header.jpg",
-    "playerHistory": []
+    id: 1838970,
+    name: "Crush the Castle Legacy Collection",
+    image: null,
+    imageUrl: "https://cdn.akamai.steamstatic.com/steam/apps/1838970/header.jpg",
+    playerHistory: [],
   },
   {
-    "_id": {
-      "$oid": "629b7eb2960971fa8c05ca75"
+    _id: {
+      $oid: "629b7eb2960971fa8c05ca75",
     },
-    "id": 1838980,
-    "name": "Polyion",
-    "image": null,
-    "imageUrl": "https://cdn.akamai.steamstatic.com/steam/apps/1838980/header.jpg",
-    "playerHistory": []
-  }
+    id: 1838980,
+    name: "Polyion",
+    image: null,
+    imageUrl: "https://cdn.akamai.steamstatic.com/steam/apps/1838980/header.jpg",
+    playerHistory: [],
+  },
 ];
